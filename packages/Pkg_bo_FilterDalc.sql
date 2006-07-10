61593702b759d07809d6fe96e2f03c99209b0ab2
jeffreyk <jeffreyk>
2006-07-10 10:56:06 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@31 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- packages/Pkg_bo_FilterDalc.sql
+++ packages/Pkg_bo_FilterDalc.sql
@@ -1,5 +1,4 @@
-
-CREATE OR REPLACE package Pkg_bo_FilterDalc
+create or replace package Pkg_bo_FilterDalc
 AS
   TYPE refCur IS ref CURSOR;
 
@@ -10,7 +9,7 @@ AS
 END Pkg_bo_FilterDalc;
 /
 
-CREATE OR REPLACE package body Pkg_bo_FilterDalc
+create or replace PACKAGE BODY Pkg_Bo_Filterdalc
 IS
   PROCEDURE GetBatchProductFilter (
     cPserver_id IN CHAR,
@@ -36,7 +35,7 @@ IS
         iLupper_sku,
         iLlower_sku,
         iLCOUNT_ADV
-      FROM ya_filter_log
+      FROM YA_FILTER_LOG
       WHERE server_id = cPserver_id;
 
     EXCEPTION WHEN NO_DATA_FOUND THEN
@@ -51,36 +50,41 @@ IS
       BEGIN
         SELECT MAX(sku)
         INTO iLupper_sku
-        FROM ya_product
-        WHERE sku IN
-          (
-            SELECT pd.sku
-            FROM ya_prod_dept pd
-            WHERE pd.dept_id IN (106, 168, 5, 7448)
-          );
+        FROM YA_PRODUCT;
+--        WHERE sku IN
+--          (
+--            SELECT pd.sku
+--            FROM YA_PROD_DEPT pd
+--                  WHERE pd.dept_id IN (106, 168, 5, 7448)
+--                  WHERE dept_id > 10793 and dept_id < 10861
+--WHERE dept_id IN (10864,11118)
+
+--          );
 
         iLlower_sku := iLupper_sku;
 
-        UPDATE ya_filter_log
+        UPDATE YA_FILTER_LOG
         SET last_batch_end_datetime = SYSDATE
         WHERE server_id = cPserver_id;
       END;
     END IF;
 
-    INSERT INTO temp_sku
+    INSERT INTO TEMP_SKU
     SELECT r.sku
     FROM
       (
         SELECT sku
-        FROM ya_product
+        FROM YA_PRODUCT
         WHERE
           sku > iLupper_sku
-          AND sku IN
-            (
-              SELECT pd.sku
-              FROM ya_prod_dept pd
-              WHERE pd.dept_id IN (106, 168, 5, 7448)
-            )
+--          AND sku IN
+--            (
+--              SELECT pd.sku
+ --             FROM YA_PROD_DEPT pd
+--                  WHERE pd.dept_id IN (106, 168, 5, 7448)
+--                  WHERE dept_id > 10793 and dept_id < 10861
+--WHERE dept_id IN (10864,11118)
+--            )
         ORDER BY sku
       ) r
     WHERE ROWNUM <= iLCOUNT_ADV;
@@ -91,20 +95,22 @@ IS
       BEGIN
         iLnew_count := iLCOUNT_ADV - iLnew_count;
 
-        INSERT INTO temp_sku
+        INSERT INTO TEMP_SKU
         SELECT r.sku
         FROM
           (
             SELECT sku
-            FROM ya_product
+            FROM YA_PRODUCT
             WHERE
               sku < iLlower_sku
-              AND sku IN
-                (
-                  SELECT pd.sku
-                  FROM ya_prod_dept pd
-                  WHERE pd.dept_id IN (106, 168, 5, 7448)
-                )
+--              AND sku IN
+--                (
+--                  SELECT pd.sku
+ --                 FROM YA_PROD_DEPT pd
+--                  WHERE pd.dept_id IN (106, 168, 5, 7448)
+--                  WHERE dept_id > 10793 and dept_id < 10861
+--WHERE dept_id IN (10864,11118)
+--                )
             ORDER BY sku DESC
           ) r
         WHERE
@@ -112,8 +118,8 @@ IS
       END;
     END IF;
 
-    SELECT MAX(sku) INTO iLnew_upper FROM temp_sku;
-    SELECT MIN(sku) INTO iLnew_lower FROM temp_sku;
+    SELECT MAX(sku) INTO iLnew_upper FROM TEMP_SKU;
+    SELECT MIN(sku) INTO iLnew_lower FROM TEMP_SKU;
 
     /* update the sku within the range */
     OPEN curPgetFilter FOR
@@ -122,36 +128,82 @@ IS
       a.attribute_type_id,
       a.attribute_id
     FROM
-      ya_prod_attr pa
-      INNER JOIN ya_attribute a ON
+      YA_PROD_ATTR pa
+      INNER JOIN YA_ATTRIBUTE a ON
         pa.attribute_id = a.attribute_id
     WHERE
-      pa.sku IN (SELECT t.sku FROM temp_sku t)
+      pa.sku IN (SELECT t.sku FROM TEMP_SKU t)
       AND pa.attribute_id IN
         (
+		
 113, 117, 118, 119, 120, 121, 122, 123, 124, 156, 157, 189, 190, 191, 192, 193,
 194, 195, 196, 197, 198, 199, 200, 360, 4, 510, 511, 512, 513, 772, 773, 78372,
 78959, 80350, 80413, 80467, 80637, 80677, 80679, 80685, 80702, 80703, 80704,
 80729, 80730, 80731, 80732, 80733, 80734, 80735, 80736, 80737, 80738, 80739,
-80740, 80745, 80746, 80747, 80814, 80818, 94
+80740, 80745, 80746, 80747, 80814, 80818, 94,
+-- Electronics Revamp 2006 March
+80997, 80998, 80999, 81000, 81001, 81002, 81003, 81004, 81005, 81006, 81007,
+81008, 81009, 80954, 80955, 80956, 80957, 80958, 80959, 80960, 80961, 80962,
+80963, 80964, 80965, 80966, 80967, 80968, 80969, 80970, 80918, 80919, 80920,
+80921, 80922, 80923, 80924, 80971, 80972, 80973, 80974, 80975, 80976, 80977,
+80978, 80979, 80980, 80981, 80982, 80983, 80984, 80985, 80938, 80939, 80940,
+80941, 80942, 80943, 80944, 80945, 80946, 80947, 80948, 80949, 80950, 80951,
+80952, 80953, 80928, 80929, 80930, 80931, 80932, 80933, 80934, 80935, 80936,
+80937, 81015, 81016, 81017, 81018, 81019, 81020, 81021, 81022, 81023, 81024,
+81025, 81026, 81010, 81011, 81012, 81013, 81014, 80883, 80863, 80986, 80987,
+80988, 80989, 80990, 80991, 80992, 80993, 80994, 80995, 80996,
+-- Electronics Revamp 2006 March
+
+
+-- Yes Style Start --
+81491,81492,81493,81494,81495,81496,81497,81498,
+81500,81501,81502,81503,81504,81505,81601,81602,
+--Stone--
+81506,81507,81508,81509,81511,81524,81528,81539,
+81541,81514,81634,81537,81612,81531,81512,81521,
+81522,81654
+-- Yes Style End --
         )
     ORDER BY pa.sku DESC;
 
     SELECT COUNT(1)
     INTO iLrow_count
     FROM
-      ya_prod_attr pa
-      INNER JOIN ya_attribute a ON
+      YA_PROD_ATTR pa
+      INNER JOIN YA_ATTRIBUTE a ON
         pa.attribute_id = a.attribute_id
     WHERE
-      pa.sku IN (SELECT t.sku FROM temp_sku t)
+      pa.sku IN (SELECT t.sku FROM TEMP_SKU t)
       AND pa.attribute_id IN
         (
+		
 113, 117, 118, 119, 120, 121, 122, 123, 124, 156, 157, 189, 190, 191, 192, 193,
 194, 195, 196, 197, 198, 199, 200, 360, 4, 510, 511, 512, 513, 772, 773, 78372,
 78959, 80350, 80413, 80467, 80637, 80677, 80679, 80685, 80702, 80703, 80704,
 80729, 80730, 80731, 80732, 80733, 80734, 80735, 80736, 80737, 80738, 80739,
-80740, 80745, 80746, 80747, 80814, 80818, 94
+80740, 80745, 80746, 80747, 80814, 80818, 94,
+-- Electronics Revamp 2006 March
+80997, 80998, 80999, 81000, 81001, 81002, 81003, 81004, 81005, 81006, 81007,
+81008, 81009, 80954, 80955, 80956, 80957, 80958, 80959, 80960, 80961, 80962,
+80963, 80964, 80965, 80966, 80967, 80968, 80969, 80970, 80918, 80919, 80920,
+80921, 80922, 80923, 80924, 80971, 80972, 80973, 80974, 80975, 80976, 80977,
+80978, 80979, 80980, 80981, 80982, 80983, 80984, 80985, 80938, 80939, 80940,
+80941, 80942, 80943, 80944, 80945, 80946, 80947, 80948, 80949, 80950, 80951,
+80952, 80953, 80928, 80929, 80930, 80931, 80932, 80933, 80934, 80935, 80936,
+80937, 81015, 81016, 81017, 81018, 81019, 81020, 81021, 81022, 81023, 81024,
+81025, 81026, 81010, 81011, 81012, 81013, 81014, 80883, 80863, 80986, 80987,
+80988, 80989, 80990, 80991, 80992, 80993, 80994, 80995, 80996,
+-- Electronics Revamp 2006 March
+
+
+-- Yes Style Start --
+81491,81492,81493,81494,81495,81496,81497,81498,
+81500,81501,81502,81503,81504,81505,81601,81602,
+--Stone--
+81506,81507,81508,81509,81511,81524,81528,81539,
+81541,81514,81634,81537,81612,81531,81512,81521,
+81522,81654
+-- Yes Style End --
         );
 
     /* reset the queue if no more item need to be updated */
@@ -159,15 +211,15 @@ IS
       BEGIN
         BEGIN
           SELECT upper_sku INTO iLupper_sku
-          FROM ya_filter_log
+          FROM YA_FILTER_LOG
           WHERE server_id = cPserver_id;
-        EXCEPTION WHEN no_data_found THEN
+        EXCEPTION WHEN NO_DATA_FOUND THEN
           iLupper_sku := -1;
         END;
 
         iLlower_sku := iLupper_sku;
 
-        UPDATE ya_filter_log
+        UPDATE YA_FILTER_LOG
         SET
           last_batch_start_datetime = last_batch_end_datetime,
           last_batch_end_datetime = SYSDATE
@@ -189,7 +241,7 @@ IS
       END;
     END IF;
 
-    UPDATE ya_filter_log
+    UPDATE YA_FILTER_LOG
     SET
       upper_sku = iLupper_sku,
       lower_sku = iLlower_sku
@@ -201,6 +253,5 @@ IS
       ROLLBACK;
     END IF;
   END;
-END Pkg_bo_FilterDalc;
+END Pkg_Bo_Filterdalc;
 /
-
