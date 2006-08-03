ef4fef062f223415404c5cdf1138365db2db6fb0
anital <anital>
2006-08-03 03:11:10 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@48 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- packages/Pkg_fe_PublisherAccess.sql
+++ packages/Pkg_fe_PublisherAccess.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE PACKAGE Pkg_fe_PublisherAccess
+CREATE OR REPLACE PACKAGE Pkg_Fe_Publisheraccess
 AS
   TYPE curG IS REF CURSOR;
 
@@ -9,10 +9,12 @@ AS
     iPlangId IN INT,
     curPpublisher OUT curG
   );
-END Pkg_fe_PublisherAccess;
+END Pkg_Fe_Publisheraccess;
 /
 
-CREATE OR REPLACE PACKAGE BODY Pkg_fe_PublisherAccess
+
+
+CREATE OR REPLACE PACKAGE BODY Pkg_Fe_Publisheraccess
 IS
   PROCEDURE GetPublisherSetByDept (
     iPdeptId IN INT,
@@ -39,66 +41,52 @@ IS
       IF (iPartistId > 0)   THEN
         OPEN curPpublisher FOR
           SELECT p.publisher_id,
-             pl1.publisher_name AS e_publisher_name, pl1.publisher_name_u AS e_publisher_name_u,
-             pl2.publisher_name AS publisher_name, pl2.publisher_name_u AS publisher_name_u,
-             NVL(p.country_id, 0) AS country_id
-
-          FROM ya_publisher p
-          INNER JOIN ya_publisher_artist pa ON pa.publisher_id = p.publisher_id AND pa.artist_id = iPartistId
-          INNER JOIN (
-             SELECT pl.publisher_id, pl.lang_id, pl.publisher_name, pl.publisher_name_u
-             FROM ya_publisher_lang pl
-             INNER JOIN ya_publisher_dept pd ON pd.publisher_id=pl.publisher_id AND dept_id = iPdeptId
-/*
-                AND
-                ((CASE cLsitePrefix
-                WHEN 'us' THEN pd.us_enabled
-                WHEN 'us_ws' THEN pd.us_ws_enabled
-                WHEN 'jp' THEN pd.jp_enabled
-                WHEN 'jp_ws' THEN pd.jp_ws_enabled
-                WHEN 'hk' THEN pd.hk_enabled
-                WHEN 'hk_ws' THEN pd.hk_ws_enabled
-                WHEN 'tw' THEN pd.tw_enabled
-                WHEN 'tw_ws' THEN pd.tw_ws_enabled
-                ELSE 'Y'  END)='Y')
-*/
-             WHERE
-             pl.lang_id IN (1, iPlangId)
-          ) pl1 ON pl1.publisher_id = p.publisher_id AND  pl1.lang_id=1
-          LEFT JOIN ya_publisher_lang pl2 ON p.publisher_id=pl2.publisher_id AND pl2.lang_id=iPlangId
-          ORDER BY upper(pl2.publisher_name), upper(pl1.publisher_name);
+            pl2.publisher_name AS publisher_name, pl2.publisher_name_u AS publisher_name_u,
+            pl1.publisher_name AS e_publisher_name, pl1.publisher_name_u AS e_publisher_name_u,
+            NVL(p.country_id, 0) AS country_id
+          FROM YA_PUBLISHER p
+          INNER JOIN YA_PUBLISHER_ARTIST pa ON pa.publisher_id = p.publisher_id AND pa.artist_id = iPartistId
+          INNER JOIN
+            (
+              SELECT pl.publisher_id, pl.lang_id, pl.publisher_name, pl.publisher_name_u
+              FROM YA_PUBLISHER_LANG pl
+              INNER JOIN YA_PUBLISHER_DEPT pd ON pd.publisher_id=pl.publisher_id AND dept_id = iPdeptId
+              WHERE pl.lang_id = 1
+            ) pl1 ON p.publisher_id = pl1.publisher_id
+          LEFT JOIN
+            (
+               SELECT pl.publisher_id, pl.lang_id, pl.publisher_name, pl.publisher_name_u
+               FROM YA_PUBLISHER_LANG pl
+               INNER JOIN YA_PUBLISHER_DEPT pd ON pd.publisher_id=pl.publisher_id AND dept_id = iPdeptId
+               WHERE pl.lang_id = iPlangId
+            ) pl2 ON pl1.publisher_id = pl2.publisher_id
+          ORDER BY UPPER(pl2.publisher_name), UPPER(pl1.publisher_name);
       ELSE
         OPEN curPpublisher FOR
           SELECT p.publisher_id,
-             pl1.publisher_name AS e_publisher_name, pl1.publisher_name_u AS e_publisher_name_u,
              pl2.publisher_name AS publisher_name, pl2.publisher_name_u AS publisher_name_u,
+             pl1.publisher_name AS e_publisher_name, pl1.publisher_name_u AS e_publisher_name_u,
              NVL(p.country_id, 0) AS country_id
-
-          FROM ya_publisher p
-          INNER JOIN (
-             SELECT pl.publisher_id, pl.lang_id, pl.publisher_name, pl.publisher_name_u
-             FROM ya_publisher_lang pl
-             INNER JOIN ya_publisher_dept pd ON pd.publisher_id=pl.publisher_id AND dept_id = iPdeptId
-/*
-                AND
-                ((CASE cLsitePrefix
-                WHEN 'us' THEN pd.us_enabled
-                WHEN 'us_ws' THEN pd.us_ws_enabled
-                WHEN 'jp' THEN pd.jp_enabled
-                WHEN 'jp_ws' THEN pd.jp_ws_enabled
-                WHEN 'hk' THEN pd.hk_enabled
-                WHEN 'hk_ws' THEN pd.hk_ws_enabled
-                WHEN 'tw' THEN pd.tw_enabled
-                WHEN 'tw_ws' THEN pd.tw_ws_enabled
-                ELSE 'Y'  END)='Y')
-*/
-             WHERE
-             pl.lang_id IN (1, iPlangId)
-          ) pl1 ON pl1.publisher_id = p.publisher_id AND  pl1.lang_id=1
-          LEFT JOIN ya_publisher_lang pl2 ON p.publisher_id=pl2.publisher_id AND pl2.lang_id=iPlangId
-          ORDER BY upper(pl2.publisher_name), upper(pl1.publisher_name);
+          FROM
+            YA_PUBLISHER p
+          INNER JOIN
+            (
+               SELECT pl.publisher_id, pl.lang_id, pl.publisher_name, pl.publisher_name_u
+               FROM YA_PUBLISHER_LANG pl
+               INNER JOIN YA_PUBLISHER_DEPT pd ON pd.publisher_id=pl.publisher_id AND dept_id = iPdeptId
+               WHERE
+               pl.lang_id = 1
+            ) pl1 ON p.publisher_id = pl1.publisher_id
+         LEFT JOIN
+            (
+               SELECT pl.publisher_id, pl.lang_id, pl.publisher_name, pl.publisher_name_u
+               FROM YA_PUBLISHER_LANG pl
+               INNER JOIN YA_PUBLISHER_DEPT pd ON pd.publisher_id=pl.publisher_id AND dept_id = iPdeptId
+               WHERE
+               pl.lang_id = iPlangId
+            ) pl2 ON pl1.publisher_id = pl2.publisher_id
+          ORDER BY UPPER(pl2.publisher_name), UPPER(pl1.publisher_name);
       END IF;
   END GetPublisherSetByDept;
-END Pkg_fe_PublisherAccess;
+END Pkg_Fe_Publisheraccess;
 /
-
