e379f15a91cfc51ab5dd0fc343be4bda1b4de521
jeffreyk <jeffreyk>
2006-10-09 08:10:53 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@125 e0b5c6b3-c742-46a5-91d8-c7356876a33d

---
+++ packages/Pkg_FE_CurrencyAccess.sql
@@ -0,0 +1,46 @@
+CREATE OR REPLACE package Pkg_FE_CurrencyAccess
+AS
+  TYPE refCur IS REF CURSOR;
+
+  PROCEDURE GetCurrencyRate (
+    curPresult1 OUT refCur
+  );
+  
+
+END Pkg_FE_CurrencyAccess;
+/
+
+CREATE OR REPLACE package body Pkg_FE_CurrencyAccess
+is
+  PROCEDURE GetCurrencyRate (
+    curPresult1 OUT refCur
+  )
+  AS
+   
+    
+  BEGIN
+  
+
+OPEN curPresult1 FOR
+select 
+(select code from ya_lookup where type_id=94 and code_id=cs.currency_id) as currency,
+(select code from ya_lookup where type_id=94 and code_id=er.source_currency_id) as source_currency,
+er.exchange_rate
+from ya_currency_site cs left join ya_exchange_rate er on cs.currency_id=er.currency_id
+order by priority;
+
+
+
+
+return;
+
+  
+
+  END GetCurrencyRate;
+
+END Pkg_FE_CurrencyAccess;
+/
+
+GRANT EXECUTE ON Pkg_FE_CurrencyAccess TO YA_WEB;
+GRANT EXECUTE ON Pkg_FE_CurrencyAccess TO YS_WEB;
+GRANT EXECUTE ON Pkg_FE_CurrencyAccess TO YS_STAGING;
