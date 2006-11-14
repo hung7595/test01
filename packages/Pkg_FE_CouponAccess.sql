23f461654fba809732035726b3907e394f60356e
jeffw <jeffw>
2006-11-14 02:09:21 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@158 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- packages/Pkg_FE_CouponAccess.sql
+++ packages/Pkg_FE_CouponAccess.sql
@@ -222,11 +222,21 @@ AS
 		)
 		AND (((site_id = iPsite_id or site_id=99) and iPsite_id not in (10)) or site_id = iPsite_id)
 		union
+		select c.coupon_code, c.coupon_description, c.expiration_date from ya_coupon c where coupon_code = 'EKDWROGCKL' and 1 = (
+			select 1 from ya_shopper s
+			where shopper_id = cPshopper_id
+			and created_date <= to_date('2006/11/10', 'yyyy/mm/dd')
+			and iPsite_id = 10
+			and c.coupon_used = 'N'
+			and not exists (select 1 from OrderInfo oi inner join BillingInfo bo on oi.id = bo.orderid where oi.customerid = s.shopper_id and bo.coupon = 'EKDWROGCKL')
+		)
+		union
 		select c.coupon_code, c.coupon_description, c.expiration_date from ya_coupon c where coupon_code = 'CTCZHGOKPZ' and 1 = (
 			select 1 from ya_shopper s
 			where shopper_id = cPshopper_id
 			and created_date <= to_date('2006/10/11', 'yyyy/mm/dd')
 			and iPsite_id = 10
+			and c.coupon_used = 'N'
 			and not exists (select 1 from OrderInfo oi inner join BillingInfo bo on oi.id = bo.orderid where oi.customerid = s.shopper_id and bo.coupon = 'CTCZHGOKPZ')
 		)		ORDER BY expiration_date;
 	END GetShopperCoupon;
