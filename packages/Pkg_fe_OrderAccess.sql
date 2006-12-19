b4a95f946019942c7b07dc9807e139ce5f1f681f
jeffw <jeffw>
2006-12-19 02:12:46 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@200 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- packages/Pkg_fe_OrderAccess.sql
+++ packages/Pkg_fe_OrderAccess.sql
@@ -91,7 +91,23 @@ AS
     iPorder_num IN OUT INT,
     cPtransaction_id IN VARCHAR2 DEFAULT NULL
   );
-
+PROCEDURE InsertPaypalOrderXml (
+	cPguid IN CHAR,
+    cPshopper_id IN CHAR,
+    iPsite_id IN INT,
+    vcPcoupon_code IN VARCHAR2,
+    clobPorder_xml IN CLOB,
+    iPbill_profile_id IN INT,
+    iPship_profile_id IN INT,
+    iPcc_profile_id IN INT,
+    nPcredit_amount IN NUMBER,
+    vcPlimited_sku_csv IN VARCHAR2,
+    vcPlimited_qty_csv IN VARCHAR2,
+    iPlang_type IN INT,
+    iPencryptionKey_id IN INT,
+    iPorder_num IN OUT INT,
+    cPtransaction_id IN VARCHAR2 DEFAULT NULL
+  );
   /* proc_fe_InsertPaypalOrderXml_encrypted */
   PROCEDURE InsertPaypalOrderXmlEncrypted (
     cPshopper_id IN CHAR,
@@ -2065,7 +2081,283 @@ AS
   END InsertOrderXmlEncrypted;
 
 
+  PROCEDURE InsertPaypalOrderXml (
+	cPguid IN CHAR,
+    cPshopper_id IN CHAR,
+    iPsite_id IN INT,
+    vcPcoupon_code IN VARCHAR2,
+    clobPorder_xml IN CLOB,
+    iPbill_profile_id IN INT,
+    iPship_profile_id IN INT,
+    iPcc_profile_id IN INT,
+    nPcredit_amount IN NUMBER,
+    vcPlimited_sku_csv IN VARCHAR2,
+    vcPlimited_qty_csv IN VARCHAR2,
+    iPlang_type IN INT,
+    iPencryptionKey_id IN INT,
+    iPorder_num IN OUT INT,
+    cPtransaction_id IN VARCHAR2 DEFAULT NULL
+  )
+  AS
+  --  cLguid CHAR(32);
+    iLstatus INT;
+    iLpaypal_status INT;
+    iLexist INT;
+    iLdebit_credit_return INT;
+    iLseq_currval INT;
+    iLseq_diff INT;
+  BEGIN
+ /*   BEGIN
+      SELECT paypal_uid
+      INTO cLguid
+      FROM ya_checkout_data
+      WHERE
+        shopper_id = cPshopper_id
+        AND site_id = iPsite_id;
+    EXCEPTION
+      WHEN NO_DATA_FOUND THEN
+        BEGIN
+          iPorder_num := -1;
+          RETURN;
+        END;
+    END;*/
 
+    BEGIN
+      SELECT status, paypal_status
+      INTO iLstatus, iLpaypal_status
+      FROM ya_paypal_order_mapping
+      WHERE payment_uid = cPguid;
+    EXCEPTION
+      WHEN NO_DATA_FOUND THEN
+        BEGIN
+          iPorder_num := -1;
+          RETURN;
+        END;
+    END;
+
+
+    IF iLstatus IN (-1,0,1,8) AND iLpaypal_status IN (-1,1,4) THEN
+      BEGIN
+        IF iPorder_num IS NULL OR iPorder_num < 0 THEN
+          SELECT SEQ_order.NEXTVAL INTO iPorder_num FROM DUAL;
+        ELSE
+          SELECT SEQ_order.NEXTVAL INTO iLseq_currval FROM dual;
+          iLseq_diff := iPorder_num - iLseq_currval;
+          IF iLseq_diff <> 0 THEN
+            EXECUTE IMMEDIATE 'ALTER SEQUENCE SEQ_order INCREMENT BY ' || iLseq_diff;
+            SELECT SEQ_order.NEXTVAL INTO iLseq_currval FROM dual;
+            EXECUTE IMMEDIATE 'ALTER SEQUENCE SEQ_order INCREMENT BY 1';
+          END IF;
+        END IF;
+
+        INSERT INTO ya_order
+          (
+            ORDER_NUM,
+            shopper_id,
+            site_id,
+            coupon_code,
+            order_xml,
+            encryptionKey
+          )
+        VALUES
+          (
+            iPorder_num,
+            cPshopper_id,
+            iPsite_id,
+            vcPcoupon_code,
+            clobPorder_xml,
+            iPencryptionKey_id
+          );
+
+        SELECT COUNT(1)
+        INTO iLexist
+        FROM
+          YA_NEW_BASKET_SHADOW b
+          INNER JOIN YA_BARGAIN_PRODUCT p ON
+            b.sku = p.sku
+            AND b.site_id = p.site_id
+        WHERE
+          b.shopper_id = cPshopper_id
+          AND b.site_id = iPsite_id
+		  AND b.paypal_uid=cPguid
+          AND b.TYPE = 0;
+
+        IF iLexist > 0 THEN
+          BEGIN
+            INSERT INTO ya_order_sales_detail
+              (
+                order_num,
+                sku,
+                remark
+              )
+            SELECT
+              iPorder_num,
+              b.sku,
+              availability
+            FROM
+              YA_NEW_BASKET_SHADOW b
+              INNER JOIN YA_BARGAIN_PRODUCT p ON
+                b.sku = p.sku
+                AND p.site_id = iPsite_id
+              INNER JOIN backend_adm.productavailability pa ON
+                b.sku = pa.productid
+                AND pa.regionid = iPsite_id
+                AND pa.category = 1
+            WHERE
+              b.shopper_id = cPshopper_id
+              AND b.site_id = iPsite_id
+			  AND b.paypal_uid=cPguid
+              AND b.TYPE = 0;
+          END;
+        END IF;
+
+		DELETE FROM YA_NEW_BASKET_SHADOW
+        WHERE
+          shopper_id = cPshopper_id
+          AND site_id = iPsite_id
+		  AND paypal_uid=cPguid
+          AND TYPE = 0;
+
+        DELETE FROM YA_WARRANTY_BASKET_shadow
+        WHERE
+          shopper_id = cPshopper_id
+          AND site_id = iPsite_id
+		  AND paypal_uid=cPguid;
+
+        DELETE FROM ya_checkout_data_shadow
+        WHERE
+          shopper_id = cPshopper_id
+          AND site_id = iPsite_id
+		  AND paypal_uid=cPguid;
+		
+        DELETE FROM YA_NEW_BASKET
+        WHERE
+          shopper_id = cPshopper_id
+          AND site_id = iPsite_id
+          AND TYPE = 0;
+
+        DELETE FROM YA_WARRANTY_BASKET
+        WHERE
+          shopper_id = cPshopper_id
+          AND site_id = iPsite_id;
+
+        DELETE FROM ya_checkout_data
+        WHERE
+          shopper_id = cPshopper_id
+          AND site_id = iPsite_id;
+
+        DELETE FROM ya_giftcard_data
+        WHERE
+          shopper_id = cPshopper_id
+          AND site_id = iPsite_id;
+
+        IF iPbill_profile_id <> -1 THEN
+          BEGIN
+            UPDATE YA_ADDRESS
+            SET preferred_bill = 'N'
+            WHERE
+              shopper_id = cPshopper_id
+              AND preferred_bill = 'Y';
+
+            UPDATE YA_ADDRESS
+            SET preferred_bill = 'Y'
+            WHERE
+              shopper_id = cPshopper_id
+              AND address_id = iPbill_profile_id;
+          END;
+        END IF;
+
+        IF iPship_profile_id <> -1 THEN
+          BEGIN
+            UPDATE YA_ADDRESS
+            SET preferred_ship = 'N'
+            WHERE
+              shopper_id = cPshopper_id
+              AND preferred_ship = 'Y'
+              AND site_id = iPsite_id;
+
+            UPDATE YA_ADDRESS
+            SET preferred_ship = 'Y'
+            WHERE
+              address_id = iPship_profile_id
+              AND shopper_id = cPshopper_id;
+          END;
+        END IF;
+
+        IF iPcc_profile_id <> -1 THEN
+          BEGIN
+            UPDATE YA_CREDIT_CARD_PROFILE
+            SET preferred = 'N'
+            WHERE shopper_id = cPshopper_id;
+
+            UPDATE YA_CREDIT_CARD_PROFILE
+            SET preferred = 'Y'
+            WHERE
+              profile_id = iPcc_profile_id
+              AND shopper_id = cPshopper_id;
+          END;
+        END IF;
+
+        IF LENGTH(vcPcoupon_code) > 0 THEN
+          BEGIN
+            UPDATE YA_COUPON
+            SET coupon_used = 'Y'
+            WHERE
+              coupon_code = vcPcoupon_code
+              AND
+                (
+                  (
+                    shopper_id = cPshopper_id
+                    AND all_shoppers NOT IN ('Y','U')
+                  ) -- Y: all shoppers, U: unique
+                  OR all_shoppers = 'O'
+                );
+          END;
+        END IF;
+
+        INSERT INTO ya_customer_info
+          (
+            order_num,
+            lang_id
+          )
+        VALUES
+          (
+            iPorder_num,
+            iPlang_type
+          );
+
+        IF nPcredit_amount > 0 THEN
+          BEGIN
+            Pkg_Fe_Orderaccess.DebitCreditBySite(cPshopper_id, nPcredit_amount, iPorder_num, 'USD', iPsite_id, iLdebit_credit_return, cPtransaction_id, iPsite_id);
+          END;
+        END IF;
+
+        IF LENGTH(vcPlimited_sku_csv) > 0 THEN
+          BEGIN
+            UpdateLimitedQuantity(vcPlimited_sku_csv, vcPlimited_qty_csv, iPsite_id);
+          END;
+        END IF;
+
+        UPDATE ya_paypal_order_mapping
+        SET
+          order_num = iPorder_num,
+          updated_datetime = SYSDATE
+        WHERE payment_uid = cPguid;
+
+      EXCEPTION WHEN OTHERS THEN
+        BEGIN
+          iPorder_num := -1;
+          ROLLBACK;
+          RETURN;
+        END;
+      END;
+    END IF;
+    COMMIT;
+    RETURN;
+  END InsertPaypalOrderXml;
+
+  
+--can be removed later
   PROCEDURE InsertPaypalOrderXmlEncrypted (
     cPshopper_id IN CHAR,
     iPsite_id IN INT,
@@ -2193,6 +2485,7 @@ AS
           END;
         END IF;
 
+		
         DELETE FROM YA_NEW_BASKET
         WHERE
           shopper_id = cPshopper_id
