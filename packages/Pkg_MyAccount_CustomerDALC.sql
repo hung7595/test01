c7313858af6d721efd72a8ecc58feeccb25b900c
jeffw <jeffw>
2006-12-20 03:00:22 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@202 e0b5c6b3-c742-46a5-91d8-c7356876a33d

---
+++ packages/Pkg_MyAccount_CustomerDALC.sql
@@ -0,0 +1,24 @@
+CREATE OR REPLACE PACKAGE Pkg_MyAccount_CustomerDALC
+AS
+PROCEDURE GetSendingEmail (
+cPshopper_id IN CHAR,
+cPemail OUT CHAR
+);
+END Pkg_MyAccount_CustomerDALC;
+/
+
+CREATE OR REPLACE PACKAGE BODY Pkg_MyAccount_CustomerDALC
+AS
+PROCEDURE GetSendingEmail (
+cPshopper_id IN CHAR,
+cPemail OUT CHAR
+)
+AS
+BEGIN
+SELECT email INTO cPemail FROM ya_shopper WHERE shopper_id = cPshopper_id;
+EXCEPTION
+WHEN NO_DATA_FOUND THEN
+cPemail:=null;
+END GetSendingEmail;
+END Pkg_MyAccount_CustomerDALC;
+/
