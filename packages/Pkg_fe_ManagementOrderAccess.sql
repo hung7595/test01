7a4c7fbc9ba0fede3077ecab1ad237430b31c306
jeffw <jeffw>
2007-01-25 02:38:39 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@225 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- packages/Pkg_fe_ManagementOrderAccess.sql
+++ packages/Pkg_fe_ManagementOrderAccess.sql
@@ -55,14 +55,30 @@ IS
     cPcurrency IN VARCHAR2
   )
   AS
-  BEGIN
+    iLseq_currval INT;
+    iLseq_diff INT;
+    iLseq_actual INT;
+  BEGIN  
+    IF iPcredit_id IS NULL OR iPcredit_id < 0 THEN
+      SELECT SEQ_FRONTEND_CREDIT_SYSTEM.NEXTVAL INTO iLseq_actual FROM DUAL;
+    ELSE
+      iLseq_actual := iPcredit_id;
+      SELECT SEQ_FRONTEND_CREDIT_SYSTEM.NEXTVAL INTO iLseq_currval FROM DUAL;
+      iLseq_diff := iPcredit_id - iLseq_currval;
+      IF iLseq_diff <> 0 THEN
+        EXECUTE IMMEDIATE 'ALTER SEQUENCE SEQ_FRONTEND_CREDIT_SYSTEM INCREMENT BY ' || iLseq_diff;
+        SELECT SEQ_FRONTEND_CREDIT_SYSTEM.NEXTVAL INTO iLseq_currval FROM DUAL;
+        EXECUTE IMMEDIATE 'ALTER SEQUENCE SEQ_FRONTEND_CREDIT_SYSTEM INCREMENT BY 1';
+      END IF;
+    END IF;
+  
     INSERT INTO ya_frontend_credit_system
     ( credit_id, site_id, shopper_id, credit_code, credit_type_id, initial_balance, current_balance, currency, transaction_datetime, bogus, rowguid )
-    VALUES (iPcredit_id, iPsite_id, cPshopper_id, cPcredit_code, 2, nPamount, nPamount, cPcurrency, SYSDATE, 'N', SYS_GUID());
+    VALUES (iLseq_actual, iPsite_id, cPshopper_id, cPcredit_code, 2, nPamount, nPamount, cPcurrency, SYSDATE, 'N', SYS_GUID());
 
     INSERT INTO ya_frontend_credit_system_txn
     ( credit_id, credit_ordernum, credit_amount, snapshot_balance )
-    VALUES (iPcredit_id, iPorder_num, nPamount, nPamount);
+    VALUES (iLseq_actual, iPorder_num, nPamount, nPamount);
 
     --COMMIT;
   END RefundStoredCredit;
