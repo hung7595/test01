f466be5c6248d682b33d0aa1bb2e32b2f8209185
jeffreyk <jeffreyk>
2007-03-28 03:49:57 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@296 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- packages/Pkg_fe_OrderAccess.sql
+++ packages/Pkg_fe_OrderAccess.sql
@@ -3324,7 +3324,7 @@ PROCEDURE GetShadowOrderWithWarranty (
       INNER JOIN backend_adm.shipping_info si ON
         oi.id = si.order_info_id
       INNER JOIN backend_adm.billing_info bi ON
-        oi.id = bi.order_info_id
+        bi.id = si.order_info_id
     WHERE oi.id = iLorder_id;
 
     RETURN;
