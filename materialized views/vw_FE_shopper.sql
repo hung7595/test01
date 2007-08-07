0135088644a4a29c6315782006ca8376f996977b
jeffreyk <jeffreyk>
2007-08-07 08:24:15 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@362 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- materialized views/vw_FE_shopper.sql
+++ materialized views/vw_FE_shopper.sql
@@ -5,10 +5,13 @@ Create materialized view vw_FE_shopper
 REFRESH COMPLETE ON DEMAND
 AS 
 SELECT
-shopper_id, member_type, created_date, anonymous
+shopper_id, member_type, created_date, anonymous, yns.site_id as subscribe_site_id, yns.status as subscribe_sts
 FROM
-ya_shopper
-WHERE created_date >= to_date('21-01-2002 15:42:34', 'DD-MM-YYYY HH24:MI:SS')
-AND anonymous = 'N'
+ya_shopper yas
+LEFT JOIN
+ya_newsletter_subscriber yns ON yas.email = yns.email
+WHERE yas.created_date >= to_date('21-01-2002 15:42:34', 'DD-MM-YYYY HH24:MI:SS')
+AND yas.anonymous = 'N'
 /
 
+grant select on vw_FE_shopper to be_app;
