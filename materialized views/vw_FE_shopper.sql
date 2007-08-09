64aeaafccfbceccb08beb279ccc31693b490650b
jeffreyk <jeffreyk>
2007-08-09 09:04:35 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@363 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- materialized views/vw_FE_shopper.sql
+++ materialized views/vw_FE_shopper.sql
@@ -1,17 +1,20 @@
 drop materialized view vw_FE_shopper
 /
 
-Create materialized view vw_FE_shopper
-REFRESH COMPLETE ON DEMAND
-AS 
-SELECT
-shopper_id, member_type, created_date, anonymous, yns.site_id as subscribe_site_id, yns.status as subscribe_sts
-FROM
-ya_shopper yas
-LEFT JOIN
-ya_newsletter_subscriber yns ON yas.email = yns.email
-WHERE yas.created_date >= to_date('21-01-2002 15:42:34', 'DD-MM-YYYY HH24:MI:SS')
-AND yas.anonymous = 'N'
+CREATE MATERIALIZED VIEW vw_FE_shopper
+  REFRESH COMPLETE ON DEMAND
+    AS 
+  SELECT
+      yas.shopper_id, yas.email, yas.created_date
+    FROM
+      ya_shopper yas
+    LEFT OUTER JOIN ya_customer_profile yap 
+      ON (yas.shopper_id = yap.shopper_id)
+    WHERE 
+      yas.member_type = 1  -- B2C 
+      AND yas.created_date >= TO_DATE('21-01-2002 15:42:34', 'DD-MM-YYYY HH24:MI:SS') -- earliest order date
+      AND (yap.shopper_id IS NULL or yap.REGISTER_SITE_ID = 1 or yap.REGISTER_SITE_ID = 7) -- YA US or GB site
+      AND yas.anonymous = 'N' -- registered customers
 /
 
 grant select on vw_FE_shopper to be_app;
