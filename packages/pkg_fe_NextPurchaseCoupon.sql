00cee0d27330e70f215960139db3ed7d73f19b4a
jeffreyk <jeffreyk>
2007-10-03 01:35:12 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@381 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- packages/pkg_fe_NextPurchaseCoupon.sql
+++ packages/pkg_fe_NextPurchaseCoupon.sql
@@ -27,6 +27,7 @@ PROCEDURE CreateNextPurchaseCoupon (
   )
   AS
     iLcount INT;
+    iLcountOrder INT;
 	cLcoupon_code_YA VARCHAR2(8);
 	cLcoupon_code_YS VARCHAR2(8);
 	cLyaCampaignName VARCHAR2(50);
@@ -35,60 +36,66 @@ PROCEDURE CreateNextPurchaseCoupon (
 	cLysCouponDescription VARCHAR2(80);
 	cLfrontendOrderNum INT;
   BEGIN
-	IF iPsiteId <=7 THEN
-		CASE iPsiteId
-		WHEN 1 THEN -- US
-			cLyaCampaignName := 'YA + YS US$5 Coupon Campaign 2007 (US Site)';
-		WHEN 7 THEN -- GB
-			cLyaCampaignName := 'YA + YS US$5 Coupon Campaign 2007 (GB Site)';
+	-- To check coupons created before
+	SELECT COUNT(*) INTO iLcountOrder FROM ya_next_purchase_coupon WHERE order_id = iPorder_id;
+	IF iLcountOrder = 0 THEN
+		IF iPsiteId <=7 THEN
+			CASE iPsiteId
+			WHEN 1 THEN -- US
+				cLyaCampaignName := 'YA + YS US$5 Coupon Campaign 2007 (US Site)';
+			WHEN 7 THEN -- GB
+				cLyaCampaignName := 'YA + YS US$5 Coupon Campaign 2007 (GB Site)';
+			ELSE
+				cLyaCampaignName := 'YA + YS US$5 Coupon Campaign 2007';
+			END CASE;
+			
+			SELECT ORIGIN_ORDER_ID INTO cLfrontendOrderNum FROM order_info WHERE id= iPorder_id;
+			cLysCampaignName := 'YA + YS US$5 Coupon Campaign 2007 (YS Site)';
+			cLyaCouponDescription := '(YA) YesAsia.com US$5 Next Purchase Coupon (OrderNum:' || cLfrontendOrderNum  || ')';
+			cLysCouponDescription := '(YS) YesStyle.com US$5 Next Purchase Coupon (OrderNum:' || cLfrontendOrderNum || ')';
+				
+			--============================ Generate Coupon_YA=========================================
+			SELECT cast(dbms_random.string('U', 8) AS VARCHAR2(8)) INTO cLcoupon_code_YA FROM dual;
+
+			-- Make sure unique coupon code
+			SELECT count(1) INTO iLcount FROM ya_coupon WHERE coupon_code = cLcoupon_code_YA;
+			WHILE (iLcount = 1)
+				LOOP
+						SELECT cast(dbms_random.string('U', 8) AS VARCHAR2(8)) INTO cLcoupon_code_YA FROM dual;
+						SELECT count(1) INTO iLcount FROM ya_coupon WHERE coupon_code = cLcoupon_code_YA;
+				END LOOP;
+			--============================ Generate Coupon_YS=========================================
+			SELECT cast(dbms_random.string('U', 8) AS VARCHAR2(8)) INTO cLcoupon_code_YS FROM dual;
+
+			-- Make sure unique coupon code
+			SELECT count(1) INTO iLcount FROM ya_coupon WHERE coupon_code = cLcoupon_code_YS;
+			WHILE (iLcount = 1)
+				LOOP
+						SELECT cast(dbms_random.string('U', 8) AS VARCHAR2(8)) INTO cLcoupon_code_YS FROM dual;
+						SELECT count(1) INTO iLcount FROM ya_coupon WHERE coupon_code = cLcoupon_code_YS;
+				END LOOP;
+			--============================ Create YA coupon=========================================
+			INSERT INTO ya_coupon
+				(shopper_id, coupon_code, campaign_name, coupon_description, dollar_coupon_value,expiration_date, all_shoppers,
+		  coupon_used, coupon_type_id, site_id, order_amount_trigger, create_id, CREATE_DATE)
+				SELECT cPshopper_id, cLcoupon_code_YA, cLyaCampaignName, cLyaCouponDescription, 5, add_months(SYSDATE, 2), 'N', 'N', 1, iPsiteId, 5, 'next_purchase', SYSDATE
+			FROM dual;
+			--============================ Create YS coupon=========================================
+			INSERT INTO ya_coupon
+				(shopper_id, coupon_code, campaign_name, coupon_description, dollar_coupon_value,expiration_date, all_shoppers,
+		  coupon_used, coupon_type_id, site_id, order_amount_trigger, create_id, CREATE_DATE)
+				SELECT cPshopper_id, cLcoupon_code_YS, cLysCampaignName, cLysCouponDescription, 5, add_months(SYSDATE, 2), 'N', 'N', 1, 10, 5, 'next_purchase', SYSDATE
+			FROM dual;
+			--============================ Keep record for sending email===============================
+			INSERT INTO ya_next_purchase_coupon (coupon_code, order_id) 
+				VALUES (cLcoupon_code_YS, iPorder_id);
+			INSERT INTO ya_next_purchase_coupon (coupon_code, order_id) 
+				VALUES (cLcoupon_code_YA, iPorder_id);
+				
+			SELECT count(*) INTO iPreturn FROM ya_coupon WHERE coupon_code = cLcoupon_code_YS OR coupon_code = cLcoupon_code_YA;
 		ELSE
-			cLyaCampaignName := 'YA + YS US$5 Coupon Campaign 2007';
-		END CASE;
-		
-		SELECT ORIGIN_ORDER_ID INTO cLfrontendOrderNum FROM order_info WHERE id= iPorder_id;
-		cLysCampaignName := 'YA + YS US$5 Coupon Campaign 2007 (YS Site)';
-		cLyaCouponDescription := '(YA) YesAsia.com US$5 Next Purchase Coupon (OrderNum:' || cLfrontendOrderNum  || ')';
-		cLysCouponDescription := '(YS) YesStyle.com US$5 Next Purchase Coupon (OrderNum:' || cLfrontendOrderNum || ')';
-			
-		--============================ Generate Coupon_YA=========================================
-		SELECT cast(dbms_random.string('U', 8) AS VARCHAR2(8)) INTO cLcoupon_code_YA FROM dual;
-
-		-- Make sure unique coupon code
-		SELECT count(1) INTO iLcount FROM ya_coupon WHERE coupon_code = cLcoupon_code_YA;
-		WHILE (iLcount = 1)
-			LOOP
-					SELECT cast(dbms_random.string('U', 8) AS VARCHAR2(8)) INTO cLcoupon_code_YA FROM dual;
-					SELECT count(1) INTO iLcount FROM ya_coupon WHERE coupon_code = cLcoupon_code_YA;
-			END LOOP;
-		--============================ Generate Coupon_YS=========================================
-		SELECT cast(dbms_random.string('U', 8) AS VARCHAR2(8)) INTO cLcoupon_code_YS FROM dual;
-
-		-- Make sure unique coupon code
-		SELECT count(1) INTO iLcount FROM ya_coupon WHERE coupon_code = cLcoupon_code_YS;
-		WHILE (iLcount = 1)
-			LOOP
-					SELECT cast(dbms_random.string('U', 8) AS VARCHAR2(8)) INTO cLcoupon_code_YS FROM dual;
-					SELECT count(1) INTO iLcount FROM ya_coupon WHERE coupon_code = cLcoupon_code_YS;
-			END LOOP;
-		--============================ Create YA coupon=========================================
-		INSERT INTO ya_coupon
-			(shopper_id, coupon_code, campaign_name, coupon_description, dollar_coupon_value,expiration_date, all_shoppers,
-	  coupon_used, coupon_type_id, site_id, order_amount_trigger, create_id, CREATE_DATE)
-			SELECT cPshopper_id, cLcoupon_code_YA, cLyaCampaignName, cLyaCouponDescription, 5, add_months(SYSDATE, 2), 'N', 'N', 1, iPsiteId, 5, 'next_purchase', SYSDATE
-		FROM dual;
-		--============================ Create YS coupon=========================================
-		INSERT INTO ya_coupon
-			(shopper_id, coupon_code, campaign_name, coupon_description, dollar_coupon_value,expiration_date, all_shoppers,
-	  coupon_used, coupon_type_id, site_id, order_amount_trigger, create_id, CREATE_DATE)
-			SELECT cPshopper_id, cLcoupon_code_YS, cLysCampaignName, cLysCouponDescription, 5, add_months(SYSDATE, 2), 'N', 'N', 1, 10, 5, 'next_purchase', SYSDATE
-		FROM dual;
-		--============================ Keep record for sending email===============================
-		INSERT INTO ya_next_purchase_coupon (coupon_code, order_id) 
-			VALUES (cLcoupon_code_YS, iPorder_id);
-		INSERT INTO ya_next_purchase_coupon (coupon_code, order_id) 
-			VALUES (cLcoupon_code_YA, iPorder_id);
-			
-		SELECT count(*) INTO iPreturn FROM ya_coupon WHERE coupon_code = cLcoupon_code_YS OR coupon_code = cLcoupon_code_YA;
+			SELECT 0 INTO iPreturn FROM dual;
+		END IF;
 	ELSE
 		SELECT 0 INTO iPreturn FROM dual;
 	END IF;
@@ -96,6 +103,7 @@ PROCEDURE CreateNextPurchaseCoupon (
 	EXCEPTION WHEN OTHERS THEN
 		ROLLBACK;
   END CreateNextPurchaseCoupon;
+
 
 END Pkg_FE_NextPurchaseCoupon;
 /
