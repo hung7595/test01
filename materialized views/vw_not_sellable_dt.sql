36767cdb5135b95010d4556ecf0655419dfe0916
jeffreyk <jeffreyk>
2007-11-14 01:41:33 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@408 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- materialized views/vw_not_sellable_dt.sql
+++ materialized views/vw_not_sellable_dt.sql
@@ -1,6 +1,6 @@
 drop materialized view vw_not_sellable_dt
 /
-
+/*
 Create materialized view vw_not_sellable_dt
 REFRESH COMPLETE ON DEMAND
 AS 
@@ -10,6 +10,18 @@ WHERE ROUND(SYSDATE - a.last_non_sellable_dt) < 30
   AND (a.last_sellable_dt < a.last_non_sellable_dt
   OR  a.last_sellable_dt IS NULL)
 AND a.last_non_sellable_dt IS NOT NULL
+*/
+
+Create materialized view vw_not_sellable_dt
+REFRESH COMPLETE ON DEMAND
+AS 
+SELECT a.prod_id, a.origin_id, a.category, a.last_non_sellable_dt, a.last_sellable_dt 
+FROM prod_avlb a 
+INNER JOIN ya_product p on a.prod_id = p.sku
+WHERE ROUND(SYSDATE - a.last_non_sellable_dt) < 30 
+AND (a.last_sellable_dt < a.last_non_sellable_dt OR a.last_sellable_dt IS NULL) 
+AND a.last_non_sellable_dt IS NOT NULL
+AND p.release_date < (sysdate - 30)
 /
 
 create index ix_VW_NOT_SELLABLE_DT_01 on VW_NOT_SELLABLE_DT (prod_id);
