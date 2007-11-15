a3a106b51b292c9c5b5117711f271edac2bb5f29
michaelw <michaelw>
2007-11-15 01:48:23 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@410 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- packages/pkg_fe_myAcc_GiftCertAccess.sql
+++ packages/pkg_fe_myAcc_GiftCertAccess.sql
@@ -37,22 +37,24 @@ BEGIN
         WHEN gc.send_ecard_datetime IS NOT NULL AND gc.cert_used = 'Y' THEN 'Redeemed'
         WHEN gc.send_ecard_datetime IS NOT NULL AND gc.cert_used = 'N' THEN 'Waiting'
       END AS status,
-      gc.recipient_email
+      NVL(rc.email, gc.recipient_email) as recipient_email
   FROM ya_gift_cert gc, 
        ya_shopper s,
+       ya_shopper rc,
        order_info oi,
        order_line ol,
        order_line_dtl otd
   WHERE    
     gc.purchaser_shopper_id = iPShopperId
     AND gc.purchaser_shopper_id = s.shopper_id
+    AND gc.recipient_shopper_id = rc.shopper_id(+)
     AND oi.origin_order_id = cast(gc.order_num as nvarchar2(100))
     AND oi.id = ol.order_info_id
     AND ol.prod_id = gc.sku
     AND ol.id = otd.order_line_id
     AND otd.sts = 6
     AND gc.bogus <> 'Y'
-  ORDER BY created_date ASC;
+  ORDER BY created_date ASC;  
     
 END GetPurchasedGiftCert;
 
@@ -74,7 +76,7 @@ BEGIN
         WHEN gc.send_ecard_datetime IS NOT NULL AND gc.cert_used = 'Y' THEN 'Redeemed'
         WHEN gc.send_ecard_datetime IS NOT NULL AND gc.cert_used = 'N' THEN 'Waiting'
       END AS status,
-      gc.recipient_email
+      rc.email as recipient_email
   FROM ya_gift_cert gc, 
        ya_shopper s,
        ya_shopper rc,
