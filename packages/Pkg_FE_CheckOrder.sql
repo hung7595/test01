8a93b5a9901c455b2c8e3ab73297f5e8c16b2d6b
michaelw <michaelw>
2008-02-06 03:17:06 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@440 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- packages/Pkg_FE_CheckOrder.sql
+++ packages/Pkg_FE_CheckOrder.sql
@@ -6,9 +6,8 @@ AS
   iPendRow in int,
   curPresult1 out refCur
   );
-END Pkg_FE_CheckOrder;
+  END Pkg_FE_CheckOrder;
   /
-
 
   CREATE OR REPLACE PACKAGE BODY Pkg_FE_CheckOrder
   IS
@@ -23,24 +22,29 @@ END Pkg_FE_CheckOrder;
     iLExistValue int;
 	
   BEGIN
+	DELETE FROM temp_order_id;
     iLCounter:=iPstartRow+1;
 	delete from temp_sku;
 	WHILE (iLCounter<=iPendRow)
 	LOOP
 		select count(*) INTO iLExistValue from ya_order where order_num = iLCounter;
 		IF (iLExistValue=0) THEN
-			insert into temp_sku values(iLCounter);
+			insert into temp_order_id (order_id) values(iLCounter);
 		END IF;
 		iLCounter:=iLCounter+1;
 	END LOOP;
 
+    IF sqlcode = 0 THEN
+		COMMIT;
+	ELSE
+		ROLLBACK;
+	END IF;
+	
 	OPEN curPresult1 FOR	  
-		select * from temp_sku order by sku;
-	ROLLBACK;
+		select * from temp_order_id order by order_id;
 	RETURN;
 	
-  END GetMissingOrderId;
-  
-  END Pkg_FE_CheckOrder;
+  END GetMissingOrderId;  
+END Pkg_FE_CheckOrder;
 
 /
