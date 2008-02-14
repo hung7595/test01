a4b91d9a66503ec1b703eb4089e28183f3cf2113
jeffreyk <jeffreyk>
2008-02-14 03:58:48 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@444 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- packages/Pkg_FE_CheckOrder.sql
+++ packages/Pkg_FE_CheckOrder.sql
@@ -1,16 +1,22 @@
-CREATE OR REPLACE PACKAGE Pkg_FE_CheckOrder
-AS
+CREATE OR REPLACE PACKAGE PKG_FE_CHECKORDER AS
+
   TYPE refCur IS REF CURSOR;
   PROCEDURE GetMissingOrderId (
   iPstartRow in int,
   iPendRow in int,
   curPresult1 out refCur
   );
-  END Pkg_FE_CheckOrder;
-  /
+  PROCEDURE GetMissingOrderId2 (
+  curPresult1 out refCur
+  );
 
-  CREATE OR REPLACE PACKAGE BODY Pkg_FE_CheckOrder
-  IS
+END PKG_FE_CHECKORDER;
+
+/
+
+
+CREATE OR REPLACE PACKAGE BODY PKG_FE_CHECKORDER AS
+
   PROCEDURE GetMissingOrderId (
   iPstartRow in int,
   iPendRow in int,
@@ -22,7 +28,7 @@ AS
     iLExistValue int;
 	
   BEGIN
-	DELETE FROM temp_order_id;
+	delete from temp_order_id;
     iLCounter:=iPstartRow+1;
 	delete from temp_sku;
 	WHILE (iLCounter<=iPendRow)
@@ -45,6 +51,49 @@ AS
 	RETURN;
 	
   END GetMissingOrderId;  
-END Pkg_FE_CheckOrder;
+  
+  PROCEDURE GetMissingOrderId2 (
+  curPresult1 out refCur
+  )
+  AS  
+    iLCounter int;
+    iLstartRow int;
+    iLendRow int;
+    iLOrderId int;
+    iLExistValue int;
+	
+  BEGIN
+	delete from temp_order_id;
+        
+        select max(order_num) into iLendRow from ya_order;
+        select min(order_num) into iLstartRow from ya_order where created_datetime >= to_date(TO_CHAR(SYSDATE - 7, 'YYYYMMDD'),'YYYYMMDD');
+        
+    iLCounter:=iLstartRow;
+	delete from temp_sku;
+        IF iLstartRow>0 AND iLendRow>0 THEN
+          WHILE (iLCounter<=iLendRow)
+          LOOP
+		select count(*) INTO iLExistValue from ya_order where order_num = iLCounter;
+		IF (iLExistValue=0) THEN
+			insert into temp_order_id (order_id) values(iLCounter);
+		END IF;
+		iLCounter:=iLCounter+1;
+          END LOOP;
+        END IF;
+        
+
+    IF sqlcode = 0 THEN
+		COMMIT;
+	ELSE
+		ROLLBACK;
+	END IF;
+	
+	OPEN curPresult1 FOR	  
+		select * from temp_order_id order by order_id;
+	RETURN;
+	
+  END GetMissingOrderId2;  
+
+END PKG_FE_CHECKORDER;
 
 /
