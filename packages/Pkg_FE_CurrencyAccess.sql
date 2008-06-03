8a195ed80ab2b4f507a697b925e43e192d7ffc20
michaelw <michaelw>
2008-06-03 03:01:30 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@479 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- packages/Pkg_FE_CurrencyAccess.sql
+++ packages/Pkg_FE_CurrencyAccess.sql
@@ -1,7 +1,4 @@
-
-REM START SS_ADM PKG_FE_CURRENCYACCESS
-
-  CREATE OR REPLACE PACKAGE "SS_ADM"."PKG_FE_CURRENCYACCESS" 
+CREATE OR REPLACE PACKAGE          "PKG_FE_CURRENCYACCESS"
 AS
   TYPE refCur IS REF CURSOR;
 
@@ -9,38 +6,49 @@ AS
     curPresult1 OUT refCur
   );
 
+  PROCEDURE GetExchangeRate (
+    curPresult1 OUT refCur
+  );
 
 END Pkg_FE_CurrencyAccess;
+
 /
-CREATE OR REPLACE PACKAGE BODY "SS_ADM"."PKG_FE_CURRENCYACCESS" 
+
+
+CREATE OR REPLACE PACKAGE BODY          "PKG_FE_CURRENCYACCESS"
 is
   PROCEDURE GetCurrencyRate (
     curPresult1 OUT refCur
   )
   AS
-
-
   BEGIN
-
-
-OPEN curPresult1 FOR
-select
-(select code from ya_lookup where type_id=94 and code_id=cs.currency_id) as currency,
-(select code from ya_lookup where type_id=94 and code_id=er.source_currency_id) as source_currency,
-er.exchange_rate
-from ya_currency_site cs left join ya_exchange_rate er on cs.currency_id=er.currency_id
-order by priority;
-
-
-
-
-return;
-
-
-
+    OPEN curPresult1 FOR
+    select
+    (select code from ya_lookup where type_id=94 and code_id=cs.currency_id) as currency,
+    (select code from ya_lookup where type_id=94 and code_id=er.source_currency_id) as source_currency,
+    er.exchange_rate
+    from ya_currency_site cs left join ya_exchange_rate er on cs.currency_id=er.currency_id
+    order by priority;
+   
+    return;
   END GetCurrencyRate;
 
+  PROCEDURE GetExchangeRate (
+    curPresult1 OUT refCur
+  )
+  AS
+  BEGIN
+    OPEN curPresult1 FOR
+    select
+    (select code from ya_lookup where type_id=94 and code_id=er.currency_id) as currency,
+    (select code from ya_lookup where type_id=94 and code_id=er.source_currency_id) as source_currency,
+    er.exchange_rate
+    from ya_exchange_rate er;
+   
+    return;
+  END GetExchangeRate;
+
+
 END Pkg_FE_CurrencyAccess;
+
 /
- 
-REM END SS_ADM PKG_FE_CURRENCYACCESS
