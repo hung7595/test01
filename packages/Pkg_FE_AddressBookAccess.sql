3c318a542e4ce15659ee5e7b52c6944b7f767668
michaelw <michaelw>
2008-06-23 07:33:41 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@490 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- packages/Pkg_FE_AddressBookAccess.sql
+++ packages/Pkg_FE_AddressBookAccess.sql
@@ -16,7 +16,6 @@ AS
     iPlangId IN INT,
     curPresult OUT refCur
   );
-
   /* proc_fe_AddAddress */
   PROCEDURE AddAddress (
     cPshopper_id IN CHAR,
@@ -59,9 +58,60 @@ AS
     cPemail IN VARCHAR2,
     iPlangId IN INT
   );
+  /* proc_fe_AddAddress */
+  PROCEDURE AddAddressYS (
+    cPshopper_id IN CHAR,
+    iPsite_id IN INT,
+    cPfirst_name IN VARCHAR2,
+    cPlast_name IN VARCHAR2,
+    cPaddress1 IN VARCHAR2,
+    cPaddress2 IN VARCHAR2,
+    cPcity IN VARCHAR2,
+    iPcity_id IN INT,
+    cPstate IN VARCHAR2,
+    iPstate_id IN INT,
+    cPzip IN VARCHAR2,
+    iPcountry_id IN INT,
+    cPday_phone IN VARCHAR2,
+    cPeve_phone IN VARCHAR2,
+    cPfax_number IN VARCHAR2,
+    cPmobile_phone IN VARCHAR2,
+    cPemail IN VARCHAR2,
+    iPaddress_Id IN OUT INT
+  );
+
+  /* proc_fe_UpdateAddress */
+  PROCEDURE UpdateAddressYS (
+    iPaddress_id IN INT,
+    cPshopper_id IN CHAR,
+    iPsite_id IN INT,
+    cPfirst_name IN VARCHAR2,
+    cPlast_name IN VARCHAR2,
+    cPaddress1 IN VARCHAR2,
+    cPaddress2 IN VARCHAR2,
+    cPcity IN VARCHAR2,
+    iPcity_id IN INT,
+    cPstate IN VARCHAR2,
+    iPstate_id IN INT,
+    cPzip IN VARCHAR2,
+    iPcountry_id IN INT,
+    cPday_phone IN VARCHAR2,
+    cPeve_phone IN VARCHAR2,
+    cPfax_number IN VARCHAR2,
+    cPmobile_phone IN VARCHAR2,
+    cPemail IN VARCHAR2,
+    iPlangId IN INT
+  );
+  PROCEDURE GetChineseAddress (
+    curPresult OUT refCur
+  );
+  
+  PROCEDURE GetChineseAddressProvinces (
+    curPresult OUT refCur
+  );
+  
 END Pkg_FE_AddressBookAccess;
 /
-
 CREATE OR REPLACE PACKAGE BODY Pkg_FE_AddressBookAccess
 AS
   PROCEDURE GetAddressByShopperId (
@@ -102,7 +152,8 @@ AS
         mobile_phone,
         email,
         address_id,
-        state_id
+        state_id,
+        city_id
       FROM ya_address
       WHERE
         shopper_id = cPshopper_id
@@ -125,7 +176,8 @@ AS
         mobile_phone,
         email,
         address_id,
-        state_id
+        state_id,
+        city_id
       FROM ya_address
       WHERE
         shopper_id = cPshopper_id
@@ -176,7 +228,8 @@ AS
         mobile_phone,
         email,
         address_id,
-        state_id
+        state_id,
+        city_id
       FROM ya_address
       WHERE
         shopper_id = cPshopper_id
@@ -199,7 +252,8 @@ AS
         mobile_phone,
         email,
         address_id,
-        state_id
+        state_id,
+        city_id
       FROM ya_address
       WHERE
         shopper_id = cPshopper_id
@@ -209,8 +263,7 @@ AS
     RETURN;
   END GetAddressById;
 
-
-  PROCEDURE AddAddress (
+    PROCEDURE AddAddress (
     cPshopper_id IN CHAR,
     iPsite_id IN INT,
     cPfirst_name IN VARCHAR2,
@@ -390,6 +443,219 @@ AS
     COMMIT;
     RETURN;
   END UpdateAddress;
+
+  PROCEDURE AddAddressYS (
+    cPshopper_id IN CHAR,
+    iPsite_id IN INT,
+    cPfirst_name IN VARCHAR2,
+    cPlast_name IN VARCHAR2,
+    cPaddress1 IN VARCHAR2,
+    cPaddress2 IN VARCHAR2,
+    cPcity IN VARCHAR2,
+    iPcity_id IN INT,
+    cPstate IN VARCHAR2,
+    iPstate_id IN INT,
+    cPzip IN VARCHAR2,
+    iPcountry_id IN INT,
+    cPday_phone IN VARCHAR2,
+    cPeve_phone IN VARCHAR2,
+    cPfax_number IN VARCHAR2,
+    cPmobile_phone IN VARCHAR2,
+    cPemail IN VARCHAR2,
+    iPaddress_Id IN OUT INT
+  )
+  AS
+    iLseq_currval INT;
+    iLseq_diff INT;
+  BEGIN
+    IF iPaddress_Id < 0 THEN
+      SELECT SEQ_address.nextval INTO iPaddress_Id FROM DUAL;
+    ELSE
+      SELECT SEQ_address.nextval INTO iLseq_currval FROM dual;
+      iLseq_diff := iPaddress_id - iLseq_currval;
+      IF iLseq_diff <> 0 THEN
+        EXECUTE IMMEDIATE 'ALTER SEQUENCE SEQ_ADDRESS INCREMENT BY ' || iLseq_diff;
+        SELECT SEQ_address.nextval INTO iLseq_currval FROM dual;
+        EXECUTE IMMEDIATE 'ALTER SEQUENCE SEQ_ADDRESS INCREMENT BY 1';
+      END IF;
+    END IF;
+
+    BEGIN
+      INSERT
+      INTO ya_address
+        (
+          address_id , shopper_id, firstname, lastname, address1, address2, city, state, state_id,
+          zip, country_id, day_phone, eve_phone, fax_number, mobile_phone, email, site_id, lang_id, city_id
+        )
+      VALUES
+        (
+          iPaddress_Id , cPshopper_id, cPfirst_name, cPlast_name, cPaddress1, cPaddress2, cPcity,
+          cPstate, iPstate_id, cPzip, iPcountry_id, cPday_phone, cPeve_phone, cPfax_number,
+          cPmobile_phone, cPemail, iPsite_id, 1, iPcity_id
+        );
+
+      INSERT
+      INTO ya_address
+        (
+          address_id , shopper_id, firstname, lastname, address1, address2, city, state, state_id,
+          zip, country_id, day_phone, eve_phone, fax_number, mobile_phone, email, site_id, lang_id, city_id
+        )
+      VALUES
+        (
+          iPaddress_Id , cPshopper_id, cPfirst_name, cPlast_name, cPaddress1, cPaddress2, cPcity,
+          cPstate, iPstate_id, cPzip, iPcountry_id, cPday_phone, cPeve_phone, cPfax_number,
+          cPmobile_phone, cPemail, iPsite_id, 2, iPcity_id
+        );
+
+      INSERT
+      INTO ya_address
+        (
+          address_id , shopper_id, firstname, lastname, address1, address2, city, state, state_id,
+          zip, country_id, day_phone, eve_phone, fax_number, mobile_phone, email, site_id, lang_id, city_id
+        )
+      VALUES
+        (
+          iPaddress_Id , cPshopper_id, cPfirst_name, cPlast_name, cPaddress1, cPaddress2, cPcity,
+          cPstate, iPstate_id, cPzip, iPcountry_id, cPday_phone, cPeve_phone, cPfax_number,
+          cPmobile_phone, cPemail, iPsite_id, 3, iPcity_id
+        );
+
+      INSERT
+      INTO ya_address
+        (
+          address_id , shopper_id, firstname, lastname, address1, address2, city, state, state_id,
+          zip, country_id, day_phone, eve_phone, fax_number, mobile_phone, email, site_id, lang_id, city_id
+        )
+      VALUES
+        (
+          iPaddress_Id , cPshopper_id, cPfirst_name, cPlast_name, cPaddress1, cPaddress2, cPcity,
+          cPstate, iPstate_id, cPzip, iPcountry_id, cPday_phone, cPeve_phone, cPfax_number,
+          cPmobile_phone, cPemail, iPsite_id, 4, iPcity_id
+        );
+
+      INSERT
+      INTO ya_address
+        (
+          address_id , shopper_id, firstname, lastname, address1, address2, city, state, state_id,
+          zip, country_id, day_phone, eve_phone, fax_number, mobile_phone, email, site_id, lang_id, city_id
+        )
+      VALUES
+        (
+          iPaddress_Id , cPshopper_id, cPfirst_name, cPlast_name, cPaddress1, cPaddress2, cPcity,
+          cPstate, iPstate_id, cPzip, iPcountry_id, cPday_phone, cPeve_phone, cPfax_number,
+          cPmobile_phone, cPemail, iPsite_id, 5, iPcity_id
+        );
+
+      INSERT
+      INTO ya_address
+        (
+          address_id , shopper_id, firstname, lastname, address1, address2, city, state, state_id,
+          zip, country_id, day_phone, eve_phone, fax_number, mobile_phone, email, site_id, lang_id, city_id
+        )
+      VALUES
+        (
+          iPaddress_Id , cPshopper_id, cPfirst_name, cPlast_name, cPaddress1, cPaddress2, cPcity,
+          cPstate, iPstate_id, cPzip, iPcountry_id, cPday_phone, cPeve_phone, cPfax_number,
+          cPmobile_phone, cPemail, iPsite_id, 6, iPcity_id
+        );
+
+      COMMIT;
+/*
+    EXCEPTION WHEN others THEN
+      IF SQLCODE <> 0 THEN
+        BEGIN
+          ROLLBACK;
+          iPaddress_Id := -1;
+        END;
+      END IF;
+*/
+    END;
+    RETURN;
+  END AddAddressYS;
+
+
+  PROCEDURE UpdateAddressYS (
+    iPaddress_id IN INT,
+    cPshopper_id IN CHAR,
+    iPsite_id IN INT,
+    cPfirst_name IN VARCHAR2,
+    cPlast_name IN VARCHAR2,
+    cPaddress1 IN VARCHAR2,
+    cPaddress2 IN VARCHAR2,
+    cPcity IN VARCHAR2,
+    iPcity_id IN INT,
+    cPstate IN VARCHAR2,
+    iPstate_id IN INT,
+    cPzip IN VARCHAR2,
+    iPcountry_id IN INT,
+    cPday_phone IN VARCHAR2,
+    cPeve_phone IN VARCHAR2,
+    cPfax_number IN VARCHAR2,
+    cPmobile_phone IN VARCHAR2,
+    cPemail IN VARCHAR2,
+    iPlangId IN INT
+  )
+  AS
+  BEGIN
+    UPDATE ya_address
+    SET
+      shopper_id = cPshopper_id,
+      firstname = cPfirst_name,
+      lastname = cPlast_name,
+      address1 = cPaddress1,
+      address2 = cPaddress2,
+      city = cPcity,
+      state = cPstate,
+      state_id = iPstate_id,
+      zip = cPzip,
+      country_id = iPcountry_id,
+      day_phone = cPday_phone,
+      eve_phone = cPeve_phone,
+      fax_number = cPfax_number,
+      mobile_phone = cPmobile_phone,
+      email = cPemail,
+      site_id = iPsite_id,
+      city_id = iPcity_id
+    WHERE
+      address_id = iPaddress_id;
+
+    IF SQLCODE <> 0 THEN
+      BEGIN
+        ROLLBACK;
+      END;
+    END IF;
+    COMMIT;
+    RETURN;
+  END UpdateAddressYS;
+  
+  PROCEDURE GetChineseAddress (
+    curPresult OUT refCur
+  )
+  AS
+  BEGIN
+  OPEN curPresult FOR
+select l1.code_id,l1.meaning,l2.code_id,l2.meaning 
+from ya_lookup_lang l1, ya_lookup_lang l2, ya_chinese_address_rel, ya_lookup 
+where l2.type_id = 124 and l1.type_id=123 and provinces_id=l1.code_id and city_id=l2.code_id 
+and ya_lookup.ENABLED='Y' and ya_lookup.TYPE_ID=124 and ya_lookup.CODE_ID=l2.CODE_ID
+order by l1.code_id, l2.code_id;
+
+    RETURN;
+    
+  END GetChineseAddress;
+    PROCEDURE GetChineseAddressProvinces (
+    curPresult OUT refCur
+  )
+  AS
+  BEGIN
+  OPEN curPresult FOR
+select l1.code_id,l1.meaning 
+from ya_lookup_lang l1, ya_lookup 
+where l1.type_id=123 and ya_lookup.ENABLED='Y' and ya_lookup.TYPE_ID=123 and ya_lookup.CODE_ID=l1.CODE_ID
+order by l1.code_id;
+
+    RETURN;
+  END GetChineseAddressProvinces;
 END Pkg_FE_AddressBookAccess;
 /
-
+ 
---
+++ packages/Pkg_FE_CountryBlockAccess.sql
@@ -0,0 +1,58 @@
+create or replace PACKAGE Pkg_FE_CountryBlockAccess
+AS
+  TYPE refCur IS REF CURSOR;
+
+  PROCEDURE GetCountryBlockBySku (
+    curPsku OUT refCur
+  );
+  PROCEDURE GetCountryBlockBySupplier (
+    curPsupplier OUT refCur
+  );
+  PROCEDURE GetCountryBlockByRevAcc (
+    curPrevAccount OUT refCur
+  );
+  PROCEDURE GetCountryBlockByAttr (
+    curPAttribute OUT refCur
+  );
+  END Pkg_FE_CountryBlockAccess;
+/
+create or replace PACKAGE BODY Pkg_FE_CountryBlockAccess
+IS
+  PROCEDURE GetCountryBlockBySku (
+    curPsku OUT refCur
+  )
+  AS
+  BEGIN
+    OPEN curPsku FOR
+	  select site_id,prod_id,COUNTRY_ID from ya_ban_ship_country_prod order by site_id, prod_id,country_id;
+  END GetCountryBlockBySku;
+  
+  PROCEDURE GetCountryBlockBySupplier (
+    curPsupplier OUT refCur
+  )
+  AS
+  BEGIN
+    OPEN curPsupplier FOR
+	  select site_id,SUPPLIER_ID,COUNTRY_ID from ya_ban_ship_country_supplier order by site_id, supplier_id,country_id;
+  END GetCountryBlockBySupplier;
+  
+  PROCEDURE GetCountryBlockByRevAcc (
+    curPrevAccount OUT refCur
+  )
+  AS
+  BEGIN
+    OPEN curPrevAccount FOR
+	  select site_id,ACCOUNT_ID,COUNTRY_ID from ya_ban_ship_country_account order by site_id, account_id,country_id;
+  END GetCountryBlockByRevAcc;
+  
+  PROCEDURE GetCountryBlockByAttr (
+    curPAttribute OUT refCur
+  )
+  AS
+  BEGIN
+   OPEN curPAttribute FOR
+          select site_id,ATTRIBUTE_ID,COUNTRY_ID from ya_ban_ship_country_attr order by attribute_id,country_id;
+  END GetCountryBlockByAttr;
+
+    END Pkg_FE_CountryBlockAccess;
+/
