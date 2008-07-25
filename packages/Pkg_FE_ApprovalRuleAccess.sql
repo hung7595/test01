204ed8d958cce18a5f4fcd863b6be96fafcdf772
michaelw <michaelw>
2008-07-25 06:59:47 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@503 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- packages/Pkg_FE_ApprovalRuleAccess.sql
+++ packages/Pkg_FE_ApprovalRuleAccess.sql
@@ -22,14 +22,22 @@ AS
   )
   AS
   BEGIN
-    SELECT 1
-    INTO iPreturn
-    FROM vw_trusted_cust
-    WHERE
-      cust_id = cPshopper_id
-      AND cc_num_encrypted = cPencrypted_cc_num;
-  EXCEPTION WHEN NO_DATA_FOUND THEN
-    iPreturn := -1;
+    IF sysdate >= to_date('2008-07-25 21:00:00', 'YYYY-MM-DD HH24:MI:SS') and sysdate <= to_date('2008-07-26 02:00:00', 'YYYY-MM-DD HH24:MI:SS') THEN
+      BEGIN
+        SELECT 1 INTO iPreturn FROM DUAL;
+      END;
+    ELSE
+      BEGIN
+        SELECT 1
+        INTO iPreturn
+        FROM vw_trusted_cust
+        WHERE
+          cust_id = cPshopper_id
+          AND cc_num_encrypted = cPencrypted_cc_num;
+      EXCEPTION WHEN NO_DATA_FOUND THEN
+        iPreturn := -1;
+      END;
+    END IF;
   END IsSuccessCreditCard;
 
   PROCEDURE IsWhiteListCreditCard (
