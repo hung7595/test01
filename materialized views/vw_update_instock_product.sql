187c9bec6d0a7befb7aa49444f9bcf9df9530709
michaelw <michaelw>
2008-08-18 08:50:56 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@515 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- materialized views/vw_update_instock_product.sql
+++ materialized views/vw_update_instock_product.sql
@@ -1,9 +1,16 @@
+drop MATERIALIZED VIEW VW_UPDATE_INSTOCK_PRODUCT;	
 Create materialized view vw_update_instock_product
 REFRESH COMPLETE ON DEMAND
 AS 
 	SELECT pr.prod_id as sku, pr.region_id as site_id
-    from prod_region pr 
+    from prod_region pr
     inner join prod_avlb pa on pr.region_id = 10 and pr.prod_id = pa.prod_id and pr.region_id = pa.region_id
     WHERE pr.list_price != -1
-	AND pa.avlb = 0
-/
+    AND pa.avlb = 0
+ UNION
+ SELECT pr.prod_id as sku, 11 as site_id
+    from prod_region pr
+    inner join prod_avlb pa on pr.region_id = 10 and pr.prod_id = pa.prod_id and pr.region_id = pa.region_id
+    inner join ya_prod_dept pd on pr.prod_id = pd.sku and pd.dept_id in (11143, 11527, 11528, 11276, 11434)
+    WHERE pr.list_price != -1
+    AND pa.avlb = 0;
