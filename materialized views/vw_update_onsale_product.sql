527373fd4dabc31912361e371b33584a776f578e
michaelw <michaelw>
2008-08-21 04:10:47 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@520 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- materialized views/vw_update_onsale_product.sql
+++ materialized views/vw_update_onsale_product.sql
@@ -4,7 +4,7 @@ drop materialized view vw_update_onsale_product
 CREATE MATERIALIZED VIEW vw_update_onsale_product
   REFRESH COMPLETE ON DEMAND
     AS 
-SELECT pr.prod_id, pr.region_id, pr.sale_price, round(1-(pr.sale_price/pr.list_price),2) as discount
+SELECT pr.prod_id as sku, pr.region_id as site_id, pr.sale_price, round(1-(pr.sale_price/pr.list_price),2) as discount
     from prod_region pr
     inner join prod_avlb pa on pr.region_id = 10 and pr.prod_id = pa.prod_id and pr.region_id = pa.region_id
     WHERE pr.sale_price IS NOT NULL
@@ -15,7 +15,7 @@ SELECT pr.prod_id, pr.region_id, pr.sale_price, round(1-(pr.sale_price/pr.list_p
  AND pr.sale_price_start <= sysdate
  AND pr.sale_price_end >= sysdate
 UNION
-SELECT pr.prod_id, 11 as region_id, pr.sale_price, round(1-(pr.sale_price/pr.list_price),2) as discount
+SELECT pr.prod_id as sku, 11 as site_id, pr.sale_price, round(1-(pr.sale_price/pr.list_price),2) as discount
     from prod_region pr
     inner join prod_avlb pa on pr.region_id = 10 and pr.prod_id = pa.prod_id and pr.region_id = pa.region_id
     inner join ya_prod_dept pd on pr.prod_id = pd.sku and pd.dept_id in (11143, 11527, 11528, 11276, 11434)
--- packages/Pkg_fe_MyAccountAddressAccess.sql
+++ packages/Pkg_fe_MyAccountAddressAccess.sql
@@ -8,11 +8,13 @@ As
     cPaddress1 IN VARCHAR2,
     cPaddress2 IN VARCHAR2,
     cPcity IN VARCHAR2,
+    iPcity_id IN INT,
     cPstate IN VARCHAR2,
     iPstate_id IN INT,
     cPzip IN VARCHAR2,
     iPcountry_id IN VARCHAR2,
     cPphone IN VARCHAR2,
+    cPevePhone IN VARCHAR2,
     cPfax IN VARCHAR2,
     cPemail IN VARCHAR2,
     iPlang_id IN INT,
@@ -28,17 +30,18 @@ As
     cPaddress1 IN VARCHAR2,
     cPaddress2 IN VARCHAR2,
     cPcity IN VARCHAR2,
+    iPcity_id IN INT,
     cPstate IN VARCHAR2,
     iPstate_id IN INT,
     cPzip IN VARCHAR2,
     iPcountry_id IN VARCHAR2,
     cPphone IN VARCHAR2,
+    cPevePhone IN VARCHAR2,
     cPfax IN VARCHAR2,
     cPemail IN VARCHAR2
   );
 END Pkg_fe_MyAccountAddressAccess;
 /
-
 CREATE OR REPLACE package body Pkg_fe_MyAccountAddressAccess
 IS
   PROCEDURE AddAddress(
@@ -49,11 +52,13 @@ IS
     cPaddress1 IN VARCHAR2,
     cPaddress2 IN VARCHAR2,
     cPcity IN VARCHAR2,
+    iPcity_id IN INT,
     cPstate IN VARCHAR2,
     iPstate_id IN INT,
     cPzip IN VARCHAR2,
     iPcountry_id IN VARCHAR2,
     cPphone IN VARCHAR2,
+    cPevePhone IN VARCHAR2,
     cPfax IN VARCHAR2,
     cPemail IN VARCHAR2,
     iPlang_id IN INT,
@@ -70,34 +75,34 @@ IS
     END IF;
 
     INSERT INTO ya_address (address_id, lang_id, shopper_id, address_profile_name, firstname,
-      lastname, address1, address2, city, state, state_id, zip, country_id, day_phone, fax_number, email, site_id, rowguid)
+      lastname, address1, address2, city, city_id, state, state_id, zip, country_id, day_phone, eve_phone, fax_number, email, site_id, rowguid)
     VALUES
       (iLaddress_current, 1, cPshopper_id, cPprofile_name, cPfirstname, cPlastname, cPaddress1,
-      cPaddress2, cPcity, cPstate, iPstate_id, cPzip, iPcountry_id, cPphone, cPfax, cPemail, iPsite_id, sys_guid());
+      cPaddress2, cPcity, iPcity_id, cPstate, iPstate_id, cPzip, iPcountry_id, cPphone, cPevePhone, cPfax, cPemail, iPsite_id, sys_guid());
 
     INSERT INTO ya_address (address_id, lang_id, shopper_id, address_profile_name, firstname,
-      lastname, address1, address2, city, state, state_id, zip, country_id, day_phone, fax_number, email, site_id, rowguid)
+      lastname, address1, address2, city, city_id, state, state_id, zip, country_id, day_phone, eve_phone, fax_number, email, site_id, rowguid)
     VALUES
       (iLaddress_current, 2, cPshopper_id, cPprofile_name, cPfirstname, cPlastname, cPaddress1,
-      cPaddress2, cPcity, cPstate, iPstate_id, cPzip, iPcountry_id, cPphone, cPfax, cPemail, iPsite_id, sys_guid());
+      cPaddress2, cPcity, iPcity_id,  cPstate, iPstate_id, cPzip, iPcountry_id, cPphone, cPevePhone, cPfax, cPemail, iPsite_id, sys_guid());
 
     INSERT INTO ya_address (address_id, lang_id, shopper_id, address_profile_name, firstname,
-      lastname, address1, address2, city, state, state_id, zip, country_id, day_phone, fax_number, email, site_id, rowguid)
+      lastname, address1, address2, city, city_id, state, state_id, zip, country_id, day_phone, eve_phone, fax_number, email, site_id, rowguid)
     VALUES
       (iLaddress_current, 3, cPshopper_id, cPprofile_name, cPfirstname, cPlastname, cPaddress1,
-      cPaddress2, cPcity, cPstate, iPstate_id, cPzip, iPcountry_id, cPphone, cPfax, cPemail, iPsite_id, sys_guid());
+      cPaddress2, cPcity, iPcity_id,  cPstate, iPstate_id, cPzip, iPcountry_id, cPphone, cPevePhone, cPfax, cPemail, iPsite_id, sys_guid());
 
     INSERT INTO ya_address (address_id, lang_id, shopper_id, address_profile_name, firstname,
-      lastname, address1, address2, city, state, state_id, zip, country_id, day_phone, fax_number, email, site_id, rowguid)
+      lastname, address1, address2, city, city_id, state, state_id, zip, country_id, day_phone, eve_phone, fax_number, email, site_id, rowguid)
     VALUES
       (iLaddress_current, 4, cPshopper_id, cPprofile_name, cPfirstname, cPlastname, cPaddress1,
-      cPaddress2, cPcity, cPstate, iPstate_id, cPzip, iPcountry_id, cPphone, cPfax, cPemail, iPsite_id, sys_guid());
+      cPaddress2, cPcity, iPcity_id,  cPstate, iPstate_id, cPzip, iPcountry_id, cPphone, cPevePhone, cPfax, cPemail, iPsite_id, sys_guid());
 
     INSERT INTO ya_address (address_id, lang_id, shopper_id, address_profile_name, firstname,
-      lastname, address1, address2, city, state, state_id, zip, country_id, day_phone, fax_number, email, site_id, rowguid)
+      lastname, address1, address2, city, city_id, state, state_id, zip, country_id, day_phone, eve_phone, fax_number, email, site_id, rowguid)
     VALUES
       (iLaddress_current, 5, cPshopper_id, cPprofile_name, cPfirstname, cPlastname, cPaddress1,
-      cPaddress2, cPcity, cPstate, iPstate_id, cPzip, iPcountry_id, cPphone, cPfax, cPemail, iPsite_id, sys_guid());
+      cPaddress2, cPcity, iPcity_id,  cPstate, iPstate_id, cPzip, iPcountry_id, cPphone, cPevePhone, cPfax, cPemail, iPsite_id, sys_guid());
     COMMIT;
   END AddAddress;
 
@@ -110,11 +115,13 @@ IS
     cPaddress1 IN VARCHAR2,
     cPaddress2 IN VARCHAR2,
     cPcity IN VARCHAR2,
+    iPcity_id IN INT,
     cPstate IN VARCHAR2,
     iPstate_id IN INT,
     cPzip IN VARCHAR2,
     iPcountry_id IN VARCHAR2,
     cPphone IN VARCHAR2,
+    cPevePhone IN VARCHAR2,
     cPfax IN VARCHAR2,
     cPemail IN VARCHAR2
   )
@@ -122,11 +129,11 @@ IS
   BEGIN
     UPDATE ya_address SET address_profile_name = cPprofile_name, firstname = cPfirstname,
       lastname = cPlastname, address1 = cPaddress1, address2 = cPaddress2,
-      city = cPcity, state = cPstate, state_id = iPstate_id, zip = cPzip, country_id = iPcountry_id,
-      day_phone = cPphone, fax_number = cPfax, email = cPemail
+      city = cPcity, city_id = iPcity_id, state = cPstate, state_id = iPstate_id, zip = cPzip, country_id = iPcountry_id,
+      day_phone = cPphone, fax_number = cPfax, email = cPemail, eve_phone = cPevePhone
     WHERE shopper_id = cPshopper_id AND address_id = iPprofile_id;
     COMMIT;
   END UpdateAddress;
 END Pkg_fe_MyAccountAddressAccess;
 /
-
+ 
