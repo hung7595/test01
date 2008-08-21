c2adcac3ad7d9177b7357a91a8b6a24c8144bf3b
michaelw <michaelw>
2008-08-21 03:24:28 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@518 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- materialized views/vw_update_onsale_product.sql
+++ materialized views/vw_update_onsale_product.sql
@@ -4,26 +4,26 @@ drop materialized view vw_update_onsale_product
 CREATE MATERIALIZED VIEW vw_update_onsale_product
   REFRESH COMPLETE ON DEMAND
     AS 
-  SELECT pr.prod_id, pr.region_id, pr.sale_price, round(1-(pr.sale_price/pr.list_price),2) as discount
+SELECT pr.prod_id, pr.region_id, pr.sale_price, round(1-(pr.sale_price/pr.list_price),2) as discount
     from prod_region pr
     inner join prod_avlb pa on pr.region_id = 10 and pr.prod_id = pa.prod_id and pr.region_id = pa.region_id
     WHERE pr.sale_price IS NOT NULL
-  AND pa.avlb < 60
-  AND pr.sale_price > 0
-  AND pr.list_price > 0
-  AND pr.list_price > pr.sale_price
-  AND pr.sale_price_start <= sysdate
-  AND pr.sale_price_end >= sysdate
- UNION
- SELECT pr.prod_id, 11 as region_id, pr.sale_price, round(1-(pr.sale_price/pr.list_price),2) as discount
-     from prod_region pr
-     inner join prod_avlb pa on pr.region_id = 10 and pr.prod_id = pa.prod_id and pr.region_id = pa.region_id
-     inner join ya_prod_dept pd on pr.prod_id = pd.sku and pd.dept_id in (11143, 11196, 11276)
-     WHERE pr.sale_price IS NOT NULL
-  AND pa.avlb < 60
-  AND pr.sale_price > 0
-  AND pr.list_price > 0
-  AND pr.list_price > pr.sale_price
-  AND pr.sale_price_start <= sysdate
-  AND pr.sale_price_end >= sysdate
+ AND pa.avlb < 60
+ AND pr.sale_price > 0
+ AND pr.list_price > 0
+ AND pr.list_price > pr.sale_price
+ AND pr.sale_price_start <= sysdate
+ AND pr.sale_price_end >= sysdate
+UNION
+SELECT pr.prod_id, 11 as region_id, pr.sale_price, round(1-(pr.sale_price/pr.list_price),2) as discount
+    from prod_region pr
+    inner join prod_avlb pa on pr.region_id = 10 and pr.prod_id = pa.prod_id and pr.region_id = pa.region_id
+    inner join ya_prod_dept pd on pr.prod_id = pd.sku and pd.dept_id in (11143, 11527, 11528, 11276, 11434)
+    WHERE pr.sale_price IS NOT NULL
+ AND pa.avlb < 60
+ AND pr.sale_price > 0
+ AND pr.list_price > 0
+ AND pr.list_price > pr.sale_price
+ AND pr.sale_price_start <= sysdate
+ AND pr.sale_price_end >= sysdate
 /
