9c05d352fead98cbc2a31c02db554af3a75c26f9
michaelw <michaelw>
2008-09-04 01:15:56 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@537 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- packages/Pkg_fe_CustomerProfileAccess.sql
+++ packages/Pkg_fe_CustomerProfileAccess.sql
@@ -80,6 +80,13 @@ AS
     iPsurvey_code		IN	INT,
     iPresult			OUT	INT
   );
+  
+  PROCEDURE UpdateNewsletterPref (
+    cPshopper_id 		IN	CHAR,
+    iPsite_id			IN	INT,
+    cPselectedCsv		 	IN	VARCHAR2,
+    cPunselectedCsv		 	IN	VARCHAR2
+  );
 	
 END Pkg_fe_CustomerProfileAccess;
 /
@@ -360,6 +367,70 @@ IS
       iPresult := 0;
  
   END GetSurveyStatusByQuestion;
+  
+  PROCEDURE UpdateNewsletterPref (
+    cPshopper_id 		IN	CHAR,
+    iPsite_id			IN	INT,
+    cPselectedCsv		 	IN	VARCHAR2,
+	cPunselectedCsv		 	IN	VARCHAR2
+  ) AS
+  iLstartAnswerPos  		INT := 1;
+  iLendAnswerPos  		INT;
+  iLtemp 			INT;
+  cLselectedCsv 		VARCHAR2(300);
+  cLunselectedCsv 		VARCHAR2(300);
+  iLNewsletterExist INT;
+  iLInExcludeList INT;
+  BEGIN
+    iLtemp := LENGTH(RTRIM(cPselectedCsv));
+    IF(iLtemp) > 1 THEN
+      BEGIN
+      SELECT 1 INTO iLInExcludeList FROM ya_reminder_exclude_list WHERE shopper_id = cPshopper_id;
+        EXCEPTION WHEN NO_DATA_FOUND THEN
+        iLInExcludeList := -1;
+      END;
+      IF(iLInExcludeList) > 0 THEN
+        DELETE FROM ya_reminder_exclude_list WHERE shopper_id = cPshopper_id;
+      END IF;
+    BEGIN
+      iLendAnswerPos := INSTR(cPselectedCsv, ',');
+      cLselectedCsv := cPselectedCsv;
+      WHILE  iLendAnswerPos  >  0
+      LOOP
+        BEGIN
+        SELECT 1 INTO iLNewsletterExist FROM ya_newsletter_subscriber WHERE shopper_id = cPshopper_id AND newsletter_id = CAST(SUBSTR(cLselectedCsv, iLstartanswerPos, iLendanswerPos - iLstartanswerPos) as INT);
+        EXCEPTION WHEN NO_DATA_FOUND THEN
+        iLNewsletterExist := -1;
+        END;
+        IF(iLNewsletterExist) > 0 THEN
+          UPDATE ya_newsletter_subscriber SET
+          status = 'A',
+          last_modified_datetime = SYSDATE
+          WHERE shopper_id = cPshopper_id AND newsletter_id = CAST(SUBSTR(cLselectedCsv, iLstartanswerPos, iLendanswerPos - iLstartanswerPos) as INT);
+        ELSE
+          INSERT INTO ya_newsletter_subscriber
+            (email,shopper_id,guid,date_time,site_id,newsletter_id,status,rowguid)
+            VALUES
+            ((SELECT email FROM ya_shopper WHERE shopper_id = cPshopper_id),cPshopper_id,sys_guid(),sysdate,iPsite_id,CAST(SUBSTR(cLselectedCsv, iLstartanswerPos, iLendanswerPos - iLstartanswerPos) as INT),'A',sys_guid());
+        END IF;
+        cLselectedCsv := SUBSTR(cLselectedCsv, iLendanswerPos + 1, LENGTH(RTRIM(cLselectedCsv)) - iLendanswerPos);
+        iLendanswerPos := INSTR(cLselectedCsv, ',');
+      END LOOP;
+    END;
+    END IF;
+    iLendAnswerPos := INSTR(cPunselectedCsv, ',');
+      cLunselectedCsv := cPunselectedCsv;
+      WHILE  iLendAnswerPos  >  0
+      LOOP
+          UPDATE ya_newsletter_subscriber SET
+          status = 'R',
+          last_modified_datetime = SYSDATE
+          WHERE shopper_id = cPshopper_id AND newsletter_id = CAST(SUBSTR(cLunselectedCsv, iLstartanswerPos, iLendanswerPos - iLstartanswerPos) as INT);
+        cLunselectedCsv := SUBSTR(cLunselectedCsv, iLendanswerPos + 1, LENGTH(RTRIM(cLunselectedCsv)) - iLendanswerPos);
+        iLendanswerPos := INSTR(cLunselectedCsv, ',');
+      END LOOP;
+    COMMIT;
+  END UpdateNewsletterPref;
 
 END Pkg_fe_CustomerProfileAccess;
 /
