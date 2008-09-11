aaaeffac9431a18c461e90addf7a3dca350d756f
michaelw <michaelw>
2008-09-11 01:44:07 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@542 e0b5c6b3-c742-46a5-91d8-c7356876a33d

---
+++ Views/viewassolinkorderlang.sql
@@ -0,0 +1,19 @@
+CREATE OR REPLACE FORCE VIEW SS_ADM.VIEWASSOLINKORDERLANG (LINK_ID, ORDER_NUM, ORDER_DATE, SKU, PRODUCT_NAME, CREDIT_STATUS, LAST_CHANGE_DATE, QUANTITY, UNIT_PRICE, COMMISSION) AS 
+  SELECT
+    alo.link_id,
+    alo.order_num,
+    alo.order_date,
+    alo.sku,
+    pl.prod_name_u AS product_name,
+    alo.credit_status,
+    alo.last_change_date,
+    alo.quantity,
+    alo.unit_price,
+    NVL(alo.credit_amount, alo.unit_price*alo.quantity*ach.comm_rate) AS commission
+FROM YA_ASSOCIATE_LINK_ORDERS alo
+inner join YA_ASSOCIATE_LINK l
+ON alo.link_id = l.link_id
+inner join YA_ASSOCIATE_COMMISSION_HIST ach
+ON ach.associate_id = l.associate_id AND TO_CHAR(alo.order_date, 'yyyy') = TO_CHAR(ach.valid_date, 'yyyy') AND TO_CHAR(alo.order_date, 'mm') = TO_CHAR(ach.valid_date, 'mm')
+inner join YA_PROD_LANG pl
+ON alo.sku = pl.sku AND pl.lang_id = 1;
---
+++ Views/ya_prod_category.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE FORCE VIEW SS_ADM.YA_PROD_CATEGORY (SKU, CAT) AS 
+  select t1.sku, mapping.map_type cat
+  from (
+       select prodAttr.sku,
+       min(nvl(mapping.priority,9999)) id
+       from ya_prod_attr prodAttr
+       left join ya_prod_type_mapper mapping
+       ON (prodAttr.attribute_id = mapping.attr_id)
+       GROUP BY prodAttr.sku
+ ) t1
+ inner join ya_prod_type_mapper mapping
+ON mapping.priority = t1.id;
