0814a1ffa9b50f117c5e3fdacbde2c430323058b
michaelw <michaelw>
2008-09-18 07:03:21 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@551 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- materialized views/vw_editor_sku_list.sql
+++ materialized views/vw_editor_sku_list.sql
@@ -1,16 +1,25 @@
-drop materialized view vw_editor_sku_list
+drop materialized view vw_editor_sku_list 
 /
 
-CREATE MATERIALIZED VIEW vw_editor_sku_list
-  REFRESH COMPLETE ON DEMAND
-    AS 
-  SELECT distinct pd.dept_id, pd.sku FROM ya_prod_dept pd
-  INNER JOIN ya_dept_rel dr on dr.parent_dept_id in (11143, 10864, 11196, 11118, 11213, 11269, 11296, 11276)
-  INNER JOIN ya_product yp on yp.sku = pd.sku and yp.release_date > sysdate-60
-  INNER JOIN prod_avlb pa on pa.prod_id = pd.sku and pa.origin_id = 10 and pa.avlb < 60
-  INNER JOIN prod_region pr on pr.prod_id = pd.sku and pr.region_id = 10 and pr.is_enabled = 'Y'
-  INNER JOIN ya_product_rating ypr on ypr.sku = pd.sku
-  INNER JOIN ya_prod_rating_lang yprl on yprl.rating_id = ypr.rating_id
-  INNER JOIN ya_review yr on yr.review_id = yprl.us_review_id and yr.review is not null
+Create materialized view VW_EDITOR_SKU_LIST
+REFRESH COMPLETE ON DEMAND
+AS 
+select distinct dr.parent_dept_id, pd.sku from ya_prod_dept pd
+inner join ya_dept_rel dr on dr.parent_dept_id in (11143, 11276, 11213, 11196, 11296)
+ inner join ya_product yp on yp.sku = pd.sku and yp.release_date > sysdate-60
+ inner join prod_avlb pa ON pa.prod_id = pd.sku and pa.origin_id = 10 and pa.avlb < 60
+ inner join prod_region pr ON pr.prod_id = pd.sku and pr.region_id = 10 and pr.is_enabled = 'Y'
+ inner join ya_product_rating ypr ON ypr.sku = pd.sku
+ inner join ya_prod_rating_lang yprl on yprl.rating_id = ypr.rating_id
+ inner join ya_review yr on yr.review_id = yprl.us_review_id and yr.review is not null
   where pd.dept_id = dr.dept_id
-  /
+ union
+select distinct dr.parent_dept_id, pd.sku from ya_prod_dept pd
+inner join ya_dept_rel dr on dr.parent_dept_id in (11118,10864)
+ inner join prod_avlb pa ON pa.prod_id = pd.sku and pa.origin_id = 10 and pa.avlb < 60
+ inner join prod_region pr ON pr.prod_id = pd.sku and pr.region_id = 10 and pr.is_enabled = 'Y'
+ inner join ya_product_rating ypr ON ypr.sku = pd.sku
+ inner join ya_prod_rating_lang yprl on yprl.rating_id = ypr.rating_id
+ inner join ya_review yr on yr.review_id = yprl.us_review_id and yr.review is not null
+  where pd.dept_id = dr.dept_id
+/
