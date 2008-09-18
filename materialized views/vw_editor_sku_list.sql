77b4222901c2bae5c393018444e246ff9d664bfc
michaelw <michaelw>
2008-09-18 07:28:22 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@552 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- materialized views/vw_editor_sku_list.sql
+++ materialized views/vw_editor_sku_list.sql
@@ -5,7 +5,7 @@ Create materialized view VW_EDITOR_SKU_LIST
 REFRESH COMPLETE ON DEMAND
 AS 
 select distinct dr.parent_dept_id, pd.sku from ya_prod_dept pd
-inner join ya_dept_rel dr on dr.parent_dept_id in (11143, 11276, 11213, 11196, 11296)
+inner join ya_dept_rel dr on dr.parent_dept_id in (11143, 11276, 11213, 11296, 11527, 11528, 11583)
  inner join ya_product yp on yp.sku = pd.sku and yp.release_date > sysdate-60
  inner join prod_avlb pa ON pa.prod_id = pd.sku and pa.origin_id = 10 and pa.avlb < 60
  inner join prod_region pr ON pr.prod_id = pd.sku and pr.region_id = 10 and pr.is_enabled = 'Y'
