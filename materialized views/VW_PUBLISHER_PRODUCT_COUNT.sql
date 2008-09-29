16bd6a4b742e68afa581dc7d1927f9adccb2837c
michaelw <michaelw>
2008-09-29 02:02:42 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@557 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- materialized views/VW_PUBLISHER_PRODUCT_COUNT.sql
+++ materialized views/VW_PUBLISHER_PRODUCT_COUNT.sql
@@ -5,13 +5,19 @@ Create materialized view VW_PUBLISHER_PRODUCT_COUNT
 REFRESH COMPLETE ON DEMAND
 AS 
 SELECT DISTINCT yp.publisher_id AS publisher_id, ypd.dept_id AS dept_id, 
-pr.origin_id AS site_id, COUNT(ypd.sku) AS product_count
+pr.origin_id AS site_id, COUNT(DISTINCT ypd.sku) AS product_count
 FROM ya_prod_dept ypd
 INNER JOIN ya_product yp ON yp.sku=ypd.sku 
-INNER JOIN prod_avlb pa ON pa.prod_id=ypd.sku AND (pa.origin_id=10 OR pa.origin_id=11) AND pa.avlb<60
-INNER JOIN prod_region pr ON pr.prod_id=ypd.sku AND (pr.origin_id=10 OR pr.origin_id=11) AND pr.is_enabled='Y'
-WHERE NOT EXISTS
-(SELECT 1 FROM ya_product_title_rel yptr WHERE yptr.product_title_child_sku=ypd.sku)
-AND yp.publisher_id IS NOT NULL 
+INNER JOIN prod_avlb pa ON pa.prod_id=ypd.sku AND (pa.origin_id=10 OR pa.origin_id=11) 
+AND pa.avlb<60
+INNER JOIN prod_region pr ON pr.prod_id=ypd.sku AND (pr.origin_id=10 OR pr.origin_id=11) 
+AND pr.is_enabled='Y'
+LEFT JOIN ya_product_title_rel yptr ON yptr.product_title_parent_sku=ypd.sku
+WHERE 
+((yptr.product_title_parent_sku=ypd.sku) OR
+(yptr.product_title_parent_sku IS NULL AND yptr.product_title_child_sku IS NULL AND pr.is_can_sell='Y'
+AND yptr.created_user IS NULL AND yptr.created_date IS NULL))
+AND NOT EXISTS (SELECT 1 FROM ya_product_title_rel yptr2 WHERE yptr2.product_title_child_sku=ypd.sku)
+AND yp.publisher_id IS NOT NULL
 GROUP BY yp.publisher_id, ypd.dept_id, pr.origin_id ORDER BY publisher_id, dept_id, site_id
 /
