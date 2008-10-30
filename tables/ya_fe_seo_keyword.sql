8fc53724c27f7586e4307eaee4fc2d51a6c97dbf
michaelw <michaelw>
2008-10-30 06:39:55 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@575 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- tables/ya_fe_seo_keyword.sql
+++ tables/ya_fe_seo_keyword.sql
@@ -1,17 +1,22 @@
-CREATE TABLE ya_fe_seo_keyword 
+CREATE TABLE Ya_Fe_Seo_Keyword 
 (
-	id                int NOT NULL ,
-	text              nvarchar2(128),
-	lang_id           int NOT NULL ,
-	enable            int NOT NULL ,
-	note              nvarchar2(512),
-	last_update       date NOT NULL ,
+	id                int                 NOT NULL ,
+	text              nvarchar2(128)      ,
+	lang_id           int                 NOT NULL ,
+	enable            int                 NOT NULL ,
+	note              nvarchar2(512)      ,
+	last_update       date NOT NULL       ,
 	last_update_user  nvarchar2(24)
 ) 
 /
 
-ALTER TABLE ya_fe_seo_keyword ADD 
-CONSTRAINT PK_fe_seo_keyword PRIMARY KEY  (id,	lang_id)
+ALTER TABLE Ya_Fe_Seo_Keyword  ADD 
+CONSTRAINT PK_Ya_Fe_Seo_Keyword PRIMARY KEY  (id, lang_id)
+USING INDEX TABLESPACE SSCommerceserver_Index
 /
 
-alter index PK_fe_seo_keyword rebuild tablespace sscommerceserver_index;
+
+CREATE PUBLIC SYNONYM Ya_Fe_Seo_Keyword  FOR Ya_Fe_Seo_Keyword 
+/
+
+
--- tables/ya_fe_seo_keyword_product_rel.sql
+++ tables/ya_fe_seo_keyword_product_rel.sql
@@ -1,11 +1,15 @@
-CREATE TABLE ya_fe_seo_keyword_product_rel
+CREATE TABLE Ya_Fe_Seo_Keyword_Product_Rel
 (
-	sku        int    NOT NULL ,
-	keyword_id int    NOT NULL 
+	sku             int       NOT NULL ,
+	keyword_id      int       NOT NULL 
 ) 
 /
 
 ALTER TABLE ya_fe_seo_keyword_product_rel
 ADD CONSTRAINT pk_fe_seo_keyword_product_rel PRIMARY KEY (sku, keyword_id)
+USING INDEX TABLESPACE SSCommerceserver_Index
 /
 
+
+CREATE PUBLIC SYNONYM Ya_Fe_Seo_Keyword_Product  FOR Ya_Fe_Seo_Keyword_Product
+/
