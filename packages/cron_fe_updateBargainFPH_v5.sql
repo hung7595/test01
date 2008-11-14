7e34de339bfd0033fc72fb59e4940f2d865c6951
michaelw <michaelw>
2008-11-14 03:15:13 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@602 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- packages/cron_fe_updateBargainFPH_v5.sql
+++ packages/cron_fe_updateBargainFPH_v5.sql
@@ -1,8 +1,10 @@
-CREATE OR REPLACE PACKAGE cron_fe_updateBargainFPH_v5
+CREATE OR REPLACE PACKAGE CRON_FE_UPDATEBARGAINFPH_V5
 AS
-  TYPE refCur IS REF CURSOR;
-	
-	PROCEDURE cron_fe_updateBargainFPH
+  TYPE refCur IS ref CURSOR;
+
+	PROCEDURE cron_fe_updateBargainFPH;
+  
+  PROCEDURE updateBargainFPH
 	(
     iPfileId IN INT,
     iPsiteId IN INT,
@@ -11,14 +13,83 @@ AS
     iProwNum IN INT,
     iPdept_preferred IN INT
 	);
-
-END cron_fe_updateBargainFPH_v5;
+END CRON_FE_UPDATEBARGAINFPH_V5;
 /
 
-CREATE OR REPLACE PACKAGE BODY cron_fe_updateBargainFPH_v5
+
+CREATE OR REPLACE PACKAGE BODY CRON_FE_UPDATEBARGAINFPH_V5
 IS
-	
-	PROCEDURE cron_fe_updateBargainFPH
+  PROCEDURE cron_fe_updateBargainFPH
+  AS
+  BEGIN
+    
+  -- V5 - Movies and Videos - All Chinese -> 106
+  Cron_fe_updateBargainFPH_v5.updateBargainFPH(234626,1,106,101,1,106); --US
+  Cron_fe_updateBargainFPH_v5.updateBargainFPH(234626,7,106,101,1,106); --GB
+  
+  -- V5 - Movies and Videos - Japan -> 6918
+  Cron_fe_updateBargainFPH_v5.updateBargainFPH(234626,1,6918,102,1,6918); --US
+  Cron_fe_updateBargainFPH_v5.updateBargainFPH(234626,7,6918,102,1,6918); --GB
+  
+  -- V5 - Movies and Videos - Korea -> 295
+  Cron_fe_updateBargainFPH_v5.updateBargainFPH(234626,1,295,103,1,295); --US
+  Cron_fe_updateBargainFPH_v5.updateBargainFPH(234626,7,295,103,1,295); --GB
+  
+  -- V5 - Movies and Videos - Western -> 8648
+  Cron_fe_updateBargainFPH_v5.updateBargainFPH(234626,1,8648,104,1,8648); --US
+  Cron_fe_updateBargainFPH_v5.updateBargainFPH(234626,7,8648,104,1,8648); --GB
+  
+  -- V5 - Music - All Chinese -> 83
+  Cron_fe_updateBargainFPH_v5.updateBargainFPH(234626,1,83,105,1,83); --US
+  Cron_fe_updateBargainFPH_v5.updateBargainFPH(234626,7,83,105,1,83); --GB
+  
+  -- V5 - Music - Japanese -> 97
+  Cron_fe_updateBargainFPH_v5.updateBargainFPH(234626,1,97,106,1,97); --US
+  Cron_fe_updateBargainFPH_v5.updateBargainFPH(234626,7,97,106,1,97); --GB
+  
+  -- V5 - Music - Korean -> 91
+  Cron_fe_updateBargainFPH_v5.updateBargainFPH(234626,1,91,107,1,91); --US
+  Cron_fe_updateBargainFPH_v5.updateBargainFPH(234626,7,91,107,1,91); --GB
+  
+  -- V5 - Concerts and MVs - All Chinese -> 163
+  Cron_fe_updateBargainFPH_v5.updateBargainFPH(234626,1,163,108,1,163); --US
+  Cron_fe_updateBargainFPH_v5.updateBargainFPH(234626,7,163,108,1,163); --GB
+  
+  -- V5 - Games - All -> 5
+  Cron_fe_updateBargainFPH_v5.updateBargainFPH(234626,1,5,109,1,5); --US
+  Cron_fe_updateBargainFPH_v5.updateBargainFPH(234626,7,5,109,1,5); --GB
+  
+  -- V5 - Toys - All -> 7153
+  Cron_fe_updateBargainFPH_v5.updateBargainFPH(234626,1,7153,110,1,7153); --US
+  Cron_fe_updateBargainFPH_v5.updateBargainFPH(234626,7,7153,110,1,7153); --GB
+  
+  -- V5 - Books - In Chinese -> 3591
+  Cron_fe_updateBargainFPH_v5.updateBargainFPH(234626,1,3591,111,1,3591); --US
+  Cron_fe_updateBargainFPH_v5.updateBargainFPH(234626,7,3591,111,1,3591); --GB
+  
+  -- V5 - Comics - In Chinese -> 2980
+  Cron_fe_updateBargainFPH_v5.updateBargainFPH(234626,1,2980,112,1,2980); --US
+  Cron_fe_updateBargainFPH_v5.updateBargainFPH(234626,7,2980,112,1,2980); --GB
+  
+  -- V5 - TV - All Chinese -> 157
+  Cron_fe_updateBargainFPH_v5.updateBargainFPH(234626,1,157,113,1,157); --US
+  Cron_fe_updateBargainFPH_v5.updateBargainFPH(234626,7,157,113,1,157); --GB
+  
+  -- V5 - TV - Japan -> 179
+  Cron_fe_updateBargainFPH_v5.updateBargainFPH(234626,1,179,114,1,179); --US
+  Cron_fe_updateBargainFPH_v5.updateBargainFPH(234626,7,179,114,1,179); --GB
+  
+  -- V5 - TV - Korea -> 2957
+  Cron_fe_updateBargainFPH_v5.updateBargainFPH(234626,1,2957,115,1,2957); --US
+  Cron_fe_updateBargainFPH_v5.updateBargainFPH(234626,7,2957,115,1,2957); --GB
+  
+  -- V5 - TV - Western -> 10961
+  Cron_fe_updateBargainFPH_v5.updateBargainFPH(234626,1,10961,116,1,10961); --US
+  Cron_fe_updateBargainFPH_v5.updateBargainFPH(234626,7,10961,116,1,10961); --GB
+
+  END cron_fe_updateBargainFPH;
+
+	PROCEDURE updateBargainFPH
 	(
     iPfileId IN INT,
     iPsiteId IN INT,
@@ -113,6 +184,8 @@ IS
           (iLnew_ProdlotId, iPfileId, iLcurr_lotLocation, iLsku, iPdept_preferred, 'Y', iLsku_Count, 'Corn Bargain FP v5', sysdate);
           
           FETCH curP_productSku INTO iLsku;
+					iLsku_Count := iLsku_Count + 1;
+					iLcurr_lotLocation := iLcurr_lotLocation + 1;
       END LOOP;
       CLOSE curP_productSku;
     END;
@@ -122,7 +195,7 @@ IS
     ELSE
       ROLLBACK;
     END IF;
-  END cron_fe_updateBargainFPH;
-	
-END cron_fe_updateBargainFPH_v5;
+  END updateBargainFPH;
+
+END CRON_FE_UPDATEBARGAINFPH_V5;
 /
