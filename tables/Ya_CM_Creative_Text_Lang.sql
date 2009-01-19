a31edd804bd36fc04676b524b345610978f7880c
michaelw <michaelw>
2009-01-19 10:26:26 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@652 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- tables/Ya_CM_Creative_Text_Lang.sql
+++ tables/Ya_CM_Creative_Text_Lang.sql
@@ -2,9 +2,14 @@ DROP TABLE Ya_CM_Creative_Text_Lang
 /
 
 CREATE TABLE Ya_CM_Creative_Text_Lang (
-	creative_id              int             NOT NULL ,
-	lang_id                  int             NOT NULL ,
-	content                  clob
+	creative_id              int               NOT NULL ,
+	lang_id                  int               NOT NULL ,
+	content                  clob              ,
+	create_user              varchar2(50)      NOT NULL ,
+	create_dt                date              NOT NULL ,
+	mod_user                 varchar2(50)      NOT NULL ,
+	mod_dt                   date              NOT NULL
+	
 )
 /
 
