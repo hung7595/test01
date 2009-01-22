f569c32c84204ca3eaf107905613c461b187db4f
michaelw <michaelw>
2009-01-22 06:38:07 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@656 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- packages/Pkg_fe_OrderAccess.sql
+++ packages/Pkg_fe_OrderAccess.sql
@@ -1112,7 +1112,7 @@ insert into ss_adm.package_log values ('PKG_FE_ORDERACCESS','DEBITCREDITBYSITE',
           FE_last_change_datetime = SYSDATE
         WHERE
           sku = iLcurrent_sku
-          AND (site_id = iPsite_id OR site_id = 99);
+          AND (site_id = iPsite_id OR site_id = 99 OR (iPsite_id = 11 and site_id = 10));
 
         IF (iLsku_pointer > 0) THEN
           BEGIN
