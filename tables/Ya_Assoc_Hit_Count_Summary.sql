5822ae4f4394b5122bbdb43330b0eb3277085b96
michaelw <michaelw>
2009-02-27 07:18:35 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@718 e0b5c6b3-c742-46a5-91d8-c7356876a33d

---
+++ tables/Ya_Assoc_Hit_Count_Summary.sql
@@ -0,0 +1,15 @@
+DROP TABLE Ya_Assoc_Hit_Count_Summary
+/
+
+CREATE TABLE Ya_Assoc_Hit_Count_Summary (
+	count_dt    date             NOT NULL,
+	hits        int              NOT NULL,
+	code        nvarchar2(15)    NOT NULL,
+	mod_user    varchar2(200)    NOT NULL,
+	mod_dt      date             NOT NULL
+)
+/
+
+
+CREATE PUBLIC SYNONYM Ya_Assoc_Hit_Count_Summary FOR Ya_Assoc_Hit_Count_Summary
+/
