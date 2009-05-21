97cc80a16e4c4994113aa10883ecff112839c890
michaelw <michaelw>
2009-05-21 04:46:49 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@759 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- materialized views/vw_update_onsale_product.sql
+++ materialized views/vw_update_onsale_product.sql
@@ -14,6 +14,7 @@ SELECT pr.prod_id as sku, pr.region_id as site_id, pr.sale_price, round(1-(pr.sa
  AND pr.list_price > pr.sale_price
  AND pr.sale_price_start <= sysdate
  AND pr.sale_price_end >= sysdate
+ AND pr.prod_id not in (1019656242, 1019656255, 1019656243, 1019656246, 1019656240, 1019656252, 1019656259, 1019656247, 1019656244, 1019656245, 1019656254, 1019656247)
 UNION
 SELECT pr.prod_id as sku, 11 as site_id, pr.sale_price, round(1-(pr.sale_price/pr.list_price),2) as discount
     from prod_region pr
@@ -26,4 +27,5 @@ SELECT pr.prod_id as sku, 11 as site_id, pr.sale_price, round(1-(pr.sale_price/p
  AND pr.list_price > pr.sale_price
  AND pr.sale_price_start <= sysdate
  AND pr.sale_price_end >= sysdate
+ AND pr.prod_id not in (1019656242, 1019656255, 1019656243, 1019656246, 1019656240, 1019656252, 1019656259, 1019656247, 1019656244, 1019656245, 1019656254, 1019656247)
 /
