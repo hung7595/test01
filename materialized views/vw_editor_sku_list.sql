66ccd22e2947459b8eb653fa62bdf4fa63da4245
jeffreyk <jeffreyk>
2009-06-22 07:09:27 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@784 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- materialized views/vw_editor_sku_list.sql
+++ materialized views/vw_editor_sku_list.sql
@@ -9,17 +9,15 @@ inner join ya_dept_rel dr on dr.parent_dept_id in (11143, 11276, 11213, 11296, 1
  inner join ya_product yp on yp.sku = pd.sku and yp.release_date > sysdate-60
  inner join prod_avlb pa ON pa.prod_id = pd.sku and pa.origin_id = 10 and pa.avlb < 60
  inner join prod_region pr ON pr.prod_id = pd.sku and pr.region_id = 10 and pr.is_enabled = 'Y'
- inner join ya_product_rating ypr ON ypr.sku = pd.sku
- inner join ya_prod_rating_lang yprl on yprl.rating_id = ypr.rating_id
- inner join ya_review yr on yr.review_id = yprl.us_review_id and yr.review is not null
+ inner join ya_profess_review ypr ON ypr.sku = pd.sku
+ inner join ya_profess_review_lang yprl on yprl.profess_review_id = ypr.id
   where pd.dept_id = dr.dept_id
  union
 select distinct dr.parent_dept_id, pd.sku from ya_prod_dept pd
 inner join ya_dept_rel dr on dr.parent_dept_id in (11118,10864)
  inner join prod_avlb pa ON pa.prod_id = pd.sku and pa.origin_id = 10 and pa.avlb < 60
  inner join prod_region pr ON pr.prod_id = pd.sku and pr.region_id = 10 and pr.is_enabled = 'Y'
- inner join ya_product_rating ypr ON ypr.sku = pd.sku
- inner join ya_prod_rating_lang yprl on yprl.rating_id = ypr.rating_id
- inner join ya_review yr on yr.review_id = yprl.us_review_id and yr.review is not null
+ inner join ya_profess_review ypr ON ypr.sku = pd.sku
+ inner join ya_profess_review_lang yprl on yprl.profess_review_id = ypr.id
   where pd.dept_id = dr.dept_id
 /
