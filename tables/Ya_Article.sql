356b833a3086a1a97c51b2e2703773dbd94ddb66
michaelw <michaelw>
2009-06-23 06:02:17 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@788 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- tables/Ya_Article.sql
+++ tables/Ya_Article.sql
@@ -7,14 +7,10 @@ CREATE TABLE Ya_Article (
 	in_tracking_no                varchar2(50)                   ,
 	out_tracking_no               varchar2(50)                   ,
 	status                        number(10)                     NOT NULL ,
-	enable                        char(1)                        NOT NULL ,
 	created_user                  varchar2(50)                   NOT NULL ,
 	created_date                  date                           NOT NULL ,
 	updated_user                  varchar2(50)                   NOT NULL ,
-	updated_date                  date                           NOT NULL ,
-	is_ya_us_enabled              varchar2(1)                    DEFAULT 'Y' NOT NULL ,
-	is_ya_gb_enabled              varchar2(1)                    DEFAULT 'Y' NOT NULL ,
-	is_ys_gb_enabled              varchar2(1)                    DEFAULT 'Y' NOT NULL
+	updated_date                  date                           NOT NULL
 )
 /
 
@@ -24,18 +20,5 @@ USING INDEX TABLESPACE SSCommerceserver_Index
 /
 
 
-CREATE INDEX IX_Article_01 ON Ya_Article(updated_date, enable, article_id) TABLESPACE SSCommerceserver_Index
-/
-
-
 CREATE PUBLIC SYNONYM Ya_Article FOR Ya_Article
 /
-
-
-create or replace TRIGGER TRI_article
-BEFORE INSERT OR UPDATE OF enable
-ON ya_article
-FOR EACH ROW
-BEGIN
-	:NEW.enable := UPPER(:NEW.enable);
-END;
