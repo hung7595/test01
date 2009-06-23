6afe37ce2b33a71442f345a8d2b47a9840114652
jeffreyk <jeffreyk>
2009-06-23 02:03:40 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@785 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- materialized views/vw_editor_sku_list.sql
+++ materialized views/vw_editor_sku_list.sql
@@ -5,19 +5,27 @@ Create materialized view VW_EDITOR_SKU_LIST
 REFRESH COMPLETE ON DEMAND
 AS 
 select distinct dr.parent_dept_id, pd.sku from ya_prod_dept pd
-inner join ya_dept_rel dr on dr.parent_dept_id in (11143, 11276, 11213, 11296, 11527, 11528, 11583)
- inner join ya_product yp on yp.sku = pd.sku and yp.release_date > sysdate-60
- inner join prod_avlb pa ON pa.prod_id = pd.sku and pa.origin_id = 10 and pa.avlb < 60
- inner join prod_region pr ON pr.prod_id = pd.sku and pr.region_id = 10 and pr.is_enabled = 'Y'
- inner join ya_profess_review ypr ON ypr.sku = pd.sku
- inner join ya_profess_review_lang yprl on yprl.profess_review_id = ypr.id
-  where pd.dept_id = dr.dept_id
- union
+  inner join ya_dept_rel dr on dr.parent_dept_id in (11143, 11276, 11213, 11296, 11527, 11528)
+  inner join ya_product yp on yp.sku = pd.sku and yp.release_date > sysdate-60
+  inner join prod_avlb pa ON pa.prod_id = pd.sku and pa.origin_id = 10 and pa.avlb < 60
+  inner join prod_region pr ON pr.prod_id = pd.sku and pr.region_id = 10 and pr.is_enabled = 'Y'
+  inner join ya_profess_review ypr ON ypr.sku = pd.sku
+  inner join ya_profess_review_lang yprl on yprl.profess_review_id = ypr.id
+where pd.dept_id = dr.dept_id
+union
 select distinct dr.parent_dept_id, pd.sku from ya_prod_dept pd
-inner join ya_dept_rel dr on dr.parent_dept_id in (11118,10864)
- inner join prod_avlb pa ON pa.prod_id = pd.sku and pa.origin_id = 10 and pa.avlb < 60
- inner join prod_region pr ON pr.prod_id = pd.sku and pr.region_id = 10 and pr.is_enabled = 'Y'
- inner join ya_profess_review ypr ON ypr.sku = pd.sku
- inner join ya_profess_review_lang yprl on yprl.profess_review_id = ypr.id
-  where pd.dept_id = dr.dept_id
+  inner join ya_dept_rel dr on dr.parent_dept_id in (11118,10864)
+  inner join prod_avlb pa ON pa.prod_id = pd.sku and pa.origin_id = 10 and pa.avlb < 60
+  inner join prod_region pr ON pr.prod_id = pd.sku and pr.region_id = 10 and pr.is_enabled = 'Y'
+  inner join ya_profess_review ypr ON ypr.sku = pd.sku
+  inner join ya_profess_review_lang yprl on yprl.profess_review_id = ypr.id
+where pd.dept_id = dr.dept_id
+union
+-- for designer section
+select distinct pd.dept_id as parent_dept_id, pd.sku from ya_prod_dept pd
+  inner join prod_avlb pa ON pa.prod_id = pd.sku and pa.origin_id = 10 and pa.avlb < 60
+  inner join prod_region pr ON pr.prod_id = pd.sku and pr.region_id = 10 and pr.is_enabled = 'Y'
+  inner join ya_profess_review ypr ON ypr.sku = pd.sku
+  inner join ya_profess_review_lang yprl on yprl.profess_review_id = ypr.id
+where pd.dept_id = 11583
 /
