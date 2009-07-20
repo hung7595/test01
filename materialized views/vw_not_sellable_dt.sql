83522b5ffe19b1454b5ce60960c82ac6f46db907
jeffreyk <jeffreyk>
2009-07-20 06:49:25 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@813 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- materialized views/vw_not_sellable_dt.sql
+++ materialized views/vw_not_sellable_dt.sql
@@ -17,7 +17,7 @@ REFRESH COMPLETE ON DEMAND
 AS 
 SELECT a.prod_id, a.origin_id, a.category, a.last_non_sellable_dt, a.last_sellable_dt 
 FROM prod_avlb a 
-INNER JOIN ya_product p on a.prod_id = p.sku and p.is_parent = 'N'
+INNER JOIN ya_product p on a.prod_id = p.sku and p.is_prod_grp_parent = 'N'
 WHERE ROUND(SYSDATE - a.last_non_sellable_dt) < 30 
 AND (a.last_sellable_dt < a.last_non_sellable_dt OR a.last_sellable_dt IS NULL) 
 AND a.last_non_sellable_dt IS NOT NULL
@@ -29,3 +29,4 @@ create index ix_VW_NOT_SELLABLE_DT_01 on VW_NOT_SELLABLE_DT (prod_id);
 Create public synonym vw_not_sellable_dt for ss_adm.vw_not_sellable_dt;
 
 grant select on vw_not_sellable_dt to ys_staging, ys_web, bo_ys;
+
