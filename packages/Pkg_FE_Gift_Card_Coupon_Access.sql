854aa4d6045427cd1822b8cb65e9c41e9b3067b1
michaelw <michaelw>
2009-08-06 06:45:30 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@831 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- packages/Pkg_FE_Gift_Card_Coupon_Access.sql
+++ packages/Pkg_FE_Gift_Card_Coupon_Access.sql
@@ -23,16 +23,22 @@ PROCEDURE CreateGiftCardCoupon (
   AS
     iLcount INT;
 	cLcoupon_code_YS VARCHAR2(8);
-	cshopper_id VARCHAR(255);	
-	exdate date;
+	cshopper_id VARCHAR(255);
+        exdate date;
+        TYPE shopperTab IS TABLE OF VARCHAR(255) INDEX BY BINARY_INTEGER; 
+        aShopper shopperTab;
+        TYPE couponTab IS TABLE OF VARCHAR2(8) INDEX BY BINARY_INTEGER; 
+        aCoupon couponTab;
+        coupon_index BINARY_INTEGER;       
   BEGIN
-		select to_date('2007/12/02', 'yyyy/mm/dd') into exdate from dual;
+		select to_date('2007/12/26', 'yyyy/mm/dd') into exdate from dual;
+                /*
 		open cursor_shopper for
-		    SELECT distinct shopper_id from ya_shopper s, temp_gift_card_email_list t where s.email=t.email;
-
+		    SELECT shopper_id from ya_shopper s, temp_gift_card_email_list t where s.email=t.email;
+                
 		fetch cursor_shopper into cshopper_id;
 		WHILE (cursor_shopper%FOUND) loop
-		BEGIN			
+		BEGIN
 			--============================ Generate Coupon_YS=========================================
 			SELECT cast(dbms_random.string('U', 8) AS VARCHAR2(8)) INTO cLcoupon_code_YS FROM dual;
 
@@ -46,12 +52,36 @@ PROCEDURE CreateGiftCardCoupon (
 			--============================ Create YS coupon=========================================
 			INSERT INTO ya_coupon
 				(shopper_id, coupon_code, campaign_name, coupon_description, dollar_coupon_value,expiration_date, all_shoppers, coupon_used, coupon_type_id, site_id, order_amount_trigger, create_id, CREATE_DATE)
-			SELECT cshopper_id, cLcoupon_code_YS, 'Gift Card Promotion', 'Holiday gift shopping $5 gift coupon', 5, exdate, 'N', 'N', 1, 10, 30, 'Gift Card Promotion', SYSDATE
+			SELECT cshopper_id, cLcoupon_code_YS, 'YS Gift eDM ', 'YS Gift eDM  $5 gift coupon', 5, exdate, 'N', 'N', 1, 10, 30, 'YS Gift eDM', SYSDATE
 			FROM dual;
+                        --DBMS_OUTPUT.PUT_LINE( cshopper_id || ',' || cemail || ',' || cLcoupon_code_YS );
 		fetch cursor_shopper into cshopper_id;
 		END;
 		end loop;
+                */
+                SELECT shopper_id, '' bulk collect into aShopper, aCoupon from ya_shopper s, temp_gift_card_email_list t where s.email=t.email;
+
+                coupon_index := aCoupon.first;
+                LOOP
+                        --============================ Generate Coupon_YS=========================================
+			-- Make sure unique coupon code
+                        LOOP
+                                SELECT cast(dbms_random.string('U', 8) AS VARCHAR2(8)) INTO cLcoupon_code_YS FROM dual;
+                                aCoupon(coupon_index) := cLcoupon_code_YS;
+                                SELECT count(1) INTO iLcount FROM ya_coupon WHERE coupon_code = cLcoupon_code_YS;
+                                EXIT WHEN iLcount != 1;
+                        END LOOP;
+                        exit when coupon_index = aCoupon.last;
+                        coupon_index := aCoupon.next(coupon_index);
+                END LOOP;
+                        
+			--============================ Create YS coupon=========================================
+                forall i in 1 .. aShopper.count
+                  INSERT INTO ya_coupon (shopper_id, coupon_code, campaign_name, coupon_description, dollar_coupon_value,expiration_date, all_shoppers, coupon_used, coupon_type_id, site_id, order_amount_trigger, create_id, CREATE_DATE)
+                  SELECT aShopper(i), aCoupon(i), 'YesStyle.com X''mas''07 eDM', 'YesStyle.com X''mas''07 $5 coupon', 5, exdate, 'N', 'N', 1, 10, 30, 'YS eDM', SYSDATE
+                  FROM dual;
 	COMMIT;
+        --DBMS_OUTPUT.PUT_LINE( 'Process commit.' );
 	EXCEPTION WHEN OTHERS THEN
 		ROLLBACK;
 	RETURN;
@@ -59,3 +89,4 @@ PROCEDURE CreateGiftCardCoupon (
 
 END Pkg_FE_GiftCardCoupon;
 /
+
