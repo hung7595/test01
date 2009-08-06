af0666272ea627794e17bffc123ef30986117cf9
michaelw <michaelw>
2009-08-06 02:44:02 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@828 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- packages/Pkg_FE_AddressBookAccess.sql
+++ packages/Pkg_FE_AddressBookAccess.sql
@@ -120,73 +120,19 @@ AS
     curPresult OUT refCur
   )
   AS
-    iLrecordExists INT := 0;
   BEGIN
-    BEGIN
-      SELECT 1 INTO iLrecordExists
-      FROM
-        ya_address
-      WHERE
-        shopper_id = cPshopper_id
-        AND lang_id = iPlangId
-        AND ROWNUM = 1;
+    OPEN curPresult FOR
+    SELECT
+      firstname,lastname,address1,address2,city,
+      state,zip,country_id,day_phone,eve_phone,
+      fax_number,mobile_phone,email,address_id,
+      state_id,city_id
+    FROM ya_address
+    WHERE shopper_id = cPshopper_id
+    ORDER BY address_id;
 
-      EXCEPTION WHEN no_data_found THEN
-        iLrecordExists := 0;
-    END;
-
-    IF iLrecordExists = 1 THEN
-      OPEN curPresult FOR
-      SELECT
-        firstname,
-        lastname,
-        address1,
-        address2,
-        city,
-        state,
-        zip,
-        country_id,
-        day_phone,
-        eve_phone,
-        fax_number,
-        mobile_phone,
-        email,
-        address_id,
-        state_id,
-        city_id
-      FROM ya_address
-      WHERE
-        shopper_id = cPshopper_id
-        AND lang_id  = iPlangId
-      ORDER BY address_id;
-    ELSE
-      OPEN curPresult FOR
-      SELECT
-        firstname,
-        lastname,
-        address1,
-        address2,
-        city,
-        state,
-        zip,
-        country_id,
-        day_phone,
-        eve_phone,
-        fax_number,
-        mobile_phone,
-        email,
-        address_id,
-        state_id,
-        city_id
-      FROM ya_address
-      WHERE
-        shopper_id = cPshopper_id
-        AND lang_id  = 1
-      ORDER BY address_id;
-    END IF;
     RETURN;
   END GetAddressByShopperId;
-
 
   PROCEDURE GetAddressById (
     cPshopper_id IN CHAR,
@@ -195,75 +141,21 @@ AS
     curPresult OUT refCur
   )
   AS
-    iLrecordExists INT := 0;
   BEGIN
-    BEGIN
-      SELECT 1 INTO iLrecordExists
-      FROM
-        ya_address
-      WHERE
-        shopper_id = cPshopper_id
-        AND address_id = iPaddress_id
-        AND lang_id = iPlangId
-        AND ROWNUM = 1;
-
-      EXCEPTION WHEN no_data_found THEN
-        iLrecordExists := 0;
-    END;
-
-    IF iLrecordExists = 1 THEN
-      OPEN curPresult FOR
-      SELECT
-        firstname,
-        lastname,
-        address1,
-        address2,
-        city,
-        state,
-        zip,
-        country_id,
-        day_phone,
-        eve_phone,
-        fax_number,
-        mobile_phone,
-        email,
-        address_id,
-        state_id,
-        city_id
-      FROM ya_address
-      WHERE
-        shopper_id = cPshopper_id
-        AND address_id = iPaddress_id
-        AND lang_id = iPlangId;
-    ELSE
-      OPEN curPresult FOR
-      SELECT
-        firstname,
-        lastname,
-        address1,
-        address2,
-        city,
-        state,
-        zip,
-        country_id,
-        day_phone,
-        eve_phone,
-        fax_number,
-        mobile_phone,
-        email,
-        address_id,
-        state_id,
-        city_id
-      FROM ya_address
-      WHERE
-        shopper_id = cPshopper_id
-        AND address_id = iPaddress_id
-        AND lang_id = 1;
-    END IF;
+    OPEN curPresult FOR
+    SELECT
+      firstname,lastname,address1,address2,city,
+      state,zip,country_id,day_phone,eve_phone,
+      fax_number,mobile_phone,email,address_id,
+      state_id,city_id
+    FROM ya_address
+    WHERE
+      shopper_id = cPshopper_id
+      AND address_id = iPaddress_id;
     RETURN;
   END GetAddressById;
 
-    PROCEDURE AddAddress (
+  PROCEDURE AddAddress (
     cPshopper_id IN CHAR,
     iPsite_id IN INT,
     cPfirst_name IN VARCHAR2,
@@ -283,20 +175,8 @@ AS
     iPaddress_Id IN OUT INT
   )
   AS
-    iLseq_currval INT;
-    iLseq_diff INT;
   BEGIN
-    IF iPaddress_Id < 0 THEN
-      SELECT SEQ_address.nextval INTO iPaddress_Id FROM DUAL;
-    ELSE
-      SELECT SEQ_address.nextval INTO iLseq_currval FROM dual;
-      iLseq_diff := iPaddress_id - iLseq_currval;
-      IF iLseq_diff <> 0 THEN
-        EXECUTE IMMEDIATE 'ALTER SEQUENCE SEQ_ADDRESS INCREMENT BY ' || iLseq_diff;
-        SELECT SEQ_address.nextval INTO iLseq_currval FROM dual;
-        EXECUTE IMMEDIATE 'ALTER SEQUENCE SEQ_ADDRESS INCREMENT BY 1';
-      END IF;
-    END IF;
+    SELECT SEQ_address.nextval INTO iPaddress_Id FROM DUAL;
 
     BEGIN
       INSERT
@@ -309,84 +189,10 @@ AS
         (
           iPaddress_Id , cPshopper_id, cPfirst_name, cPlast_name, cPaddress1, cPaddress2, cPcity,
           cPstate, iPstate_id, cPzip, iPcountry_id, cPday_phone, cPeve_phone, cPfax_number,
-          cPmobile_phone, cPemail, iPsite_id, 1
-        );
-
-      INSERT
-      INTO ya_address
-        (
-          address_id , shopper_id, firstname, lastname, address1, address2, city, state, state_id,
-          zip, country_id, day_phone, eve_phone, fax_number, mobile_phone, email, site_id, lang_id
-        )
-      VALUES
-        (
-          iPaddress_Id , cPshopper_id, cPfirst_name, cPlast_name, cPaddress1, cPaddress2, cPcity,
-          cPstate, iPstate_id, cPzip, iPcountry_id, cPday_phone, cPeve_phone, cPfax_number,
-          cPmobile_phone, cPemail, iPsite_id, 2
-        );
-
-      INSERT
-      INTO ya_address
-        (
-          address_id , shopper_id, firstname, lastname, address1, address2, city, state, state_id,
-          zip, country_id, day_phone, eve_phone, fax_number, mobile_phone, email, site_id, lang_id
-        )
-      VALUES
-        (
-          iPaddress_Id , cPshopper_id, cPfirst_name, cPlast_name, cPaddress1, cPaddress2, cPcity,
-          cPstate, iPstate_id, cPzip, iPcountry_id, cPday_phone, cPeve_phone, cPfax_number,
-          cPmobile_phone, cPemail, iPsite_id, 3
-        );
-
-      INSERT
-      INTO ya_address
-        (
-          address_id , shopper_id, firstname, lastname, address1, address2, city, state, state_id,
-          zip, country_id, day_phone, eve_phone, fax_number, mobile_phone, email, site_id, lang_id
-        )
-      VALUES
-        (
-          iPaddress_Id , cPshopper_id, cPfirst_name, cPlast_name, cPaddress1, cPaddress2, cPcity,
-          cPstate, iPstate_id, cPzip, iPcountry_id, cPday_phone, cPeve_phone, cPfax_number,
-          cPmobile_phone, cPemail, iPsite_id, 4
-        );
-
-      INSERT
-      INTO ya_address
-        (
-          address_id , shopper_id, firstname, lastname, address1, address2, city, state, state_id,
-          zip, country_id, day_phone, eve_phone, fax_number, mobile_phone, email, site_id, lang_id
-        )
-      VALUES
-        (
-          iPaddress_Id , cPshopper_id, cPfirst_name, cPlast_name, cPaddress1, cPaddress2, cPcity,
-          cPstate, iPstate_id, cPzip, iPcountry_id, cPday_phone, cPeve_phone, cPfax_number,
-          cPmobile_phone, cPemail, iPsite_id, 5
-        );
-
-      INSERT
-      INTO ya_address
-        (
-          address_id , shopper_id, firstname, lastname, address1, address2, city, state, state_id,
-          zip, country_id, day_phone, eve_phone, fax_number, mobile_phone, email, site_id, lang_id
-        )
-      VALUES
-        (
-          iPaddress_Id , cPshopper_id, cPfirst_name, cPlast_name, cPaddress1, cPaddress2, cPcity,
-          cPstate, iPstate_id, cPzip, iPcountry_id, cPday_phone, cPeve_phone, cPfax_number,
-          cPmobile_phone, cPemail, iPsite_id, 6
+          cPmobile_phone, cPemail, iPsite_id, -1
         );
 
       COMMIT;
-/*
-    EXCEPTION WHEN others THEN
-      IF SQLCODE <> 0 THEN
-        BEGIN
-          ROLLBACK;
-          iPaddress_Id := -1;
-        END;
-      END IF;
-*/
     END;
     RETURN;
   END AddAddress;
@@ -465,20 +271,8 @@ AS
     iPaddress_Id IN OUT INT
   )
   AS
-    iLseq_currval INT;
-    iLseq_diff INT;
   BEGIN
-    IF iPaddress_Id < 0 THEN
-      SELECT SEQ_address.nextval INTO iPaddress_Id FROM DUAL;
-    ELSE
-      SELECT SEQ_address.nextval INTO iLseq_currval FROM dual;
-      iLseq_diff := iPaddress_id - iLseq_currval;
-      IF iLseq_diff <> 0 THEN
-        EXECUTE IMMEDIATE 'ALTER SEQUENCE SEQ_ADDRESS INCREMENT BY ' || iLseq_diff;
-        SELECT SEQ_address.nextval INTO iLseq_currval FROM dual;
-        EXECUTE IMMEDIATE 'ALTER SEQUENCE SEQ_ADDRESS INCREMENT BY 1';
-      END IF;
-    END IF;
+    SELECT SEQ_address.nextval INTO iPaddress_Id FROM DUAL;
 
     BEGIN
       INSERT
@@ -491,88 +285,13 @@ AS
         (
           iPaddress_Id , cPshopper_id, cPfirst_name, cPlast_name, cPaddress1, cPaddress2, cPcity,
           cPstate, iPstate_id, cPzip, iPcountry_id, cPday_phone, cPeve_phone, cPfax_number,
-          cPmobile_phone, cPemail, iPsite_id, 1, iPcity_id
-        );
-
-      INSERT
-      INTO ya_address
-        (
-          address_id , shopper_id, firstname, lastname, address1, address2, city, state, state_id,
-          zip, country_id, day_phone, eve_phone, fax_number, mobile_phone, email, site_id, lang_id, city_id
-        )
-      VALUES
-        (
-          iPaddress_Id , cPshopper_id, cPfirst_name, cPlast_name, cPaddress1, cPaddress2, cPcity,
-          cPstate, iPstate_id, cPzip, iPcountry_id, cPday_phone, cPeve_phone, cPfax_number,
-          cPmobile_phone, cPemail, iPsite_id, 2, iPcity_id
-        );
-
-      INSERT
-      INTO ya_address
-        (
-          address_id , shopper_id, firstname, lastname, address1, address2, city, state, state_id,
-          zip, country_id, day_phone, eve_phone, fax_number, mobile_phone, email, site_id, lang_id, city_id
-        )
-      VALUES
-        (
-          iPaddress_Id , cPshopper_id, cPfirst_name, cPlast_name, cPaddress1, cPaddress2, cPcity,
-          cPstate, iPstate_id, cPzip, iPcountry_id, cPday_phone, cPeve_phone, cPfax_number,
-          cPmobile_phone, cPemail, iPsite_id, 3, iPcity_id
-        );
-
-      INSERT
-      INTO ya_address
-        (
-          address_id , shopper_id, firstname, lastname, address1, address2, city, state, state_id,
-          zip, country_id, day_phone, eve_phone, fax_number, mobile_phone, email, site_id, lang_id, city_id
-        )
-      VALUES
-        (
-          iPaddress_Id , cPshopper_id, cPfirst_name, cPlast_name, cPaddress1, cPaddress2, cPcity,
-          cPstate, iPstate_id, cPzip, iPcountry_id, cPday_phone, cPeve_phone, cPfax_number,
-          cPmobile_phone, cPemail, iPsite_id, 4, iPcity_id
-        );
-
-      INSERT
-      INTO ya_address
-        (
-          address_id , shopper_id, firstname, lastname, address1, address2, city, state, state_id,
-          zip, country_id, day_phone, eve_phone, fax_number, mobile_phone, email, site_id, lang_id, city_id
-        )
-      VALUES
-        (
-          iPaddress_Id , cPshopper_id, cPfirst_name, cPlast_name, cPaddress1, cPaddress2, cPcity,
-          cPstate, iPstate_id, cPzip, iPcountry_id, cPday_phone, cPeve_phone, cPfax_number,
-          cPmobile_phone, cPemail, iPsite_id, 5, iPcity_id
-        );
-
-      INSERT
-      INTO ya_address
-        (
-          address_id , shopper_id, firstname, lastname, address1, address2, city, state, state_id,
-          zip, country_id, day_phone, eve_phone, fax_number, mobile_phone, email, site_id, lang_id, city_id
-        )
-      VALUES
-        (
-          iPaddress_Id , cPshopper_id, cPfirst_name, cPlast_name, cPaddress1, cPaddress2, cPcity,
-          cPstate, iPstate_id, cPzip, iPcountry_id, cPday_phone, cPeve_phone, cPfax_number,
-          cPmobile_phone, cPemail, iPsite_id, 6, iPcity_id
+          cPmobile_phone, cPemail, iPsite_id, -1, iPcity_id
         );
 
       COMMIT;
-/*
-    EXCEPTION WHEN others THEN
-      IF SQLCODE <> 0 THEN
-        BEGIN
-          ROLLBACK;
-          iPaddress_Id := -1;
-        END;
-      END IF;
-*/
     END;
     RETURN;
   END AddAddressYS;
-
 
   PROCEDURE UpdateAddressYS (
     iPaddress_id IN INT,
@@ -634,26 +353,32 @@ AS
   AS
   BEGIN
   OPEN curPresult FOR
-select l1.code_id,l1.meaning,l2.code_id,l2.meaning 
-from ya_lookup_lang l1, ya_lookup_lang l2, ya_chinese_address_rel, ya_lookup 
-where l2.type_id = 124 and l1.type_id=123 and provinces_id=l1.code_id and city_id=l2.code_id 
-and ya_lookup.ENABLED='Y' and ya_lookup.TYPE_ID=124 and ya_lookup.CODE_ID=l2.CODE_ID
-order by l1.code_id, l2.code_id;
-
+    SELECT l1.code_id,l1.meaning,l2.code_id,l2.meaning 
+    FROM ya_lookup_lang l1, ya_lookup_lang l2, ya_chinese_address_rel, ya_lookup 
+    WHERE l2.type_id = 124 
+      AND l1.type_id=123 
+      AND provinces_id=l1.code_id 
+      AND city_id=l2.code_id 
+      AND ya_lookup.ENABLED='Y' 
+      AND ya_lookup.TYPE_ID=124 
+      AND ya_lookup.CODE_ID=l2.CODE_ID
+    ORDER BY l1.code_id, l2.code_id;
     RETURN;
-    
   END GetChineseAddress;
-    PROCEDURE GetChineseAddressProvinces (
+  
+  PROCEDURE GetChineseAddressProvinces (
     curPresult OUT refCur
   )
   AS
   BEGIN
   OPEN curPresult FOR
-select l1.code_id,l1.meaning 
-from ya_lookup_lang l1, ya_lookup 
-where l1.type_id=123 and ya_lookup.ENABLED='Y' and ya_lookup.TYPE_ID=123 and ya_lookup.CODE_ID=l1.CODE_ID
-order by l1.code_id;
-
+    SELECT l1.code_id,l1.meaning 
+    FROM ya_lookup_lang l1, ya_lookup 
+    WHERE l1.type_id=123 
+      AND ya_lookup.ENABLED='Y' 
+      AND ya_lookup.TYPE_ID=123 
+      AND ya_lookup.CODE_ID=l1.CODE_ID
+    ORDER BY l1.code_id;
     RETURN;
   END GetChineseAddressProvinces;
 END Pkg_FE_AddressBookAccess;
