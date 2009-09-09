9b15e1946252087f5b7cce811dd84be76accb6be
michaelw <michaelw>
2009-09-09 04:13:47 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@876 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- packages/Cron_fe_PopcornCampaign.sql
+++ packages/Cron_fe_PopcornCampaign.sql
@@ -15,31 +15,35 @@ CREATE OR REPLACE PACKAGE BODY "CRON_FE_POPCORNCAMPAIGN" AS
 	select a.sku from
 	(
 		select prod.sku as sku, case when sysdate between region.sale_price_start and region.sale_price_end and nvl(region.sale_price, 0) > 0 then region.sale_price else region.list_price end as sell_price 
-		from ya_product prod, prod_region region, exchange_rate rate, exchange_rate_version version
+		from ya_product prod, prod_region region, exchange_rate rate, exchange_rate_version version, ya_prod_attr p_attr
 		where rate.EXCHANGE_RATE_VERSION_ID = version.id 
 			and version.EFFECTIVE_START_DT < sysdate and version.EFFECTIVE_END_DT > sysdate and version.id != 0
 			and region.cost_currency = rate.currency
 			and prod.sku = region.prod_id
+			and p_attr.sku = prod.sku
+			and p_attr.ATTRIBUTE_ID = 117
 			and prod.weight < 201
 			and region.is_can_sell = 'Y'
 			and region.is_enabled = 'Y'
 			and region.unit_cost / nvl(rate.exchange_rate, 1) < 6.2
 			and region.region_id = 1
-			and prod.account_id in (104,14,41,7,132,33,9,103,10,34,90,11,12,160,156,150,13,4,133,394,32,396,120,155,105,134,24,25,64,159,157,63,40,26)
+			and prod.account_id in (104,14,41,7,132,33,9,103,10,34,90,11,12,156,150,13,4,133,394,32,396,120,155,105,134,24,25,64,159,157,63,40,26)
 	) a,
 	(
 		select prod.sku as sku, case when sysdate between region.sale_price_start and region.sale_price_end and nvl(region.sale_price, 0) > 0 then region.sale_price else region.list_price end as sell_price 
-		from ya_product prod, prod_region region, exchange_rate rate, exchange_rate_version version
+		from ya_product prod, prod_region region, exchange_rate rate, exchange_rate_version version, ya_prod_attr p_attr
 		where rate.EXCHANGE_RATE_VERSION_ID = version.id 
 			and version.EFFECTIVE_START_DT < sysdate and version.EFFECTIVE_END_DT > sysdate and version.id != 0
 			and region.cost_currency = rate.currency
 			and prod.sku = region.prod_id
+			and p_attr.sku = prod.sku
+			and p_attr.ATTRIBUTE_ID = 117
 			and prod.weight < 201
 			and region.is_can_sell = 'Y'
 			and region.is_enabled = 'Y'
 			and region.unit_cost / nvl(rate.exchange_rate, 1) < 6.2
 			and region.region_id = 7
-			and prod.account_id in (104,14,41,7,132,33,9,103,10,34,90,11,12,160,156,150,13,4,133,394,32,396,120,155,105,134,24,25,64,159,157,63,40,26)
+			and prod.account_id in (104,14,41,7,132,33,9,103,10,34,90,11,12,156,150,13,4,133,394,32,396,120,155,105,134,24,25,64,159,157,63,40,26)
 	) b
 	where a.sell_price > 9.99
 	and b.sell_price > 9.99
