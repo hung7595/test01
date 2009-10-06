a099ce91abe869493a7c328ae54dba7d20589966
michaelw <michaelw>
2009-10-06 02:55:16 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@913 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- tables/Ya_Hallmark_Shopper.sql
+++ tables/Ya_Hallmark_Shopper.sql
@@ -2,17 +2,17 @@ DROP TABLE Ya_Hallmark_Shopper
 /
 
 CREATE TABLE Ya_Hallmark_Shopper (
-	hallmark_member_id           varchar(32)             NOT NULL ,
 	ya_shopper_id                char(32)                NOT NULL ,
-	create_date                  date                    NOT NULL
+	hallmark_member_id           varchar2(32)            NULL ,
+	create_date                  date                    NOT NULL ,
+	hallmark_id                  varchar2(32)            NULL
 )
 /
 
 ALTER TABLE Ya_Hallmark_Shopper
-ADD CONSTRAINT PK_Hallmark_Shopper PRIMARY KEY (hallmark_member_id, ya_shopper_id)
+ADD CONSTRAINT PK_Hallmark_Shopper PRIMARY KEY (ya_shopper_id)
 USING INDEX TABLESPACE SSCommerceserver_Index
 /
-
 
 CREATE PUBLIC SYNONYM Ya_Hallmark_Shopper FOR Ya_Hallmark_Shopper
 /
