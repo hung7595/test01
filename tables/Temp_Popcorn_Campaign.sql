16ad9998d0c220137255085dbcee84a0729b5db5
michaelw <michaelw>
2009-10-06 02:02:08 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@911 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- tables/Temp_Popcorn_Campaign.sql
+++ tables/Temp_Popcorn_Campaign.sql
@@ -6,3 +6,8 @@ CREATE GLOBAL TEMPORARY TABLE Temp_Popcorn_Campaign (
 	SKU       number      NOT NULL
 ) ON COMMIT PRESERVE ROWS
 /
+
+
+ALTER TABLE Temp_Popcorn_Campaign
+ADD CONSTRAINT PK_Temp_Popcorn_Campaign PRIMARY KEY (sku)
+/
