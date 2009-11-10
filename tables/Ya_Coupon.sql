2e4382316094ab028e85bb31515b5427d2421e1a
michaelw <michaelw>
2009-11-10 14:44:20 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@951 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- tables/Ya_Coupon.sql
+++ tables/Ya_Coupon.sql
@@ -2,24 +2,25 @@ DROP TABLE Ya_Coupon
 /
 
 CREATE TABLE Ya_Coupon (
-	coupon_code                            varchar2(32)                      NOT NULL ,
-	campaign_name                          varchar2(50)                      ,        
-	coupon_description                     varchar2(255)                     ,        
-	dollar_coupon_value                    number(18,2)                      ,        
-	percentage_coupon_value                number(18,2)                      ,        
-	order_amount_trigger                   number(18,2)                      ,        
-	order_qty_trigger                      int                               ,        
-	item_sku_trigger                       int                               ,        
-	item_qty_trigger                       int                               ,        
-	expiration_date                        date                              NOT NULL ,
-	shopper_id                             char(32)                          ,        
-	all_shoppers                           char(1)                           DEFAULT 'N' NOT NULL ,
-	coupon_used                            char(1)                           DEFAULT 'N' NOT NULL ,
-	coupon_type_id                         int                               DEFAULT 0 NOT NULL ,
-	site_id                                int                               ,       
-	rowguid                                varchar2(50)                      DEFAULT SYS_GUID() NOT NULL ,
-	create_id                              varchar2(50)                      ,
-	create_date                            date
+	coupon_code                          varchar2(32)                NOT NULL ,
+	campaign_name                        varchar2(50)                ,        
+	coupon_description                   varchar2(255)               ,        
+	dollar_coupon_value                  number(18,2)                ,        
+	percentage_coupon_value              number(18,2)                ,        
+	order_amount_trigger                 number(18,2)                ,        
+	order_qty_trigger                    int                         ,        
+	item_sku_trigger                     int                         ,        
+	item_qty_trigger                     int                         ,        
+	expiration_date                      date                        NOT NULL ,
+	shopper_id                           char(32)                    ,        
+	all_shoppers                         char(1)                     DEFAULT 'N' NOT NULL ,
+	coupon_used                          char(1)                     DEFAULT 'N' NOT NULL ,
+	coupon_type_id                       int                         DEFAULT 0 NOT NULL ,
+	site_id                              int                         ,       
+	rowguid                              varchar2(50)                DEFAULT SYS_GUID() NOT NULL ,
+	create_id                            varchar2(50)                ,
+	create_date                          date                        ,
+	currency                             char(3)                     DEFAULT 'USD' NOT NULL
 )
 /
 
