baec33d9d5a125d8cacb094cc7a69e3425965395
michaelw <michaelw>
2009-12-30 07:53:16 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@991 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- packages/CRON_FE_SearchIndexAccess.sql
+++ packages/CRON_FE_SearchIndexAccess.sql
@@ -11,9 +11,9 @@ IS
 
  AS
  BEGIN
-	delete from ya_se_reindex where created_dt < trunc(sysdate) - 1 and action = 1;
+	delete from ya_se_reindex where action = 1;
 	
-	insert into ya_se_reindex (sku, action, created_dt) 
+	insert into ya_se_reindex (sku, action, mod_dt) 
 	select distinct sku, 0, sysdate from 
 	(
 		select prod_id as sku from prod_region where 
@@ -23,9 +23,29 @@ IS
 		select prod_id as sku  from prod_region where 
 		  sale_price_end >= trunc(sysdate) - 1
 		  and sale_price_end <= trunc(sysdate) + 1
+    union      
+    select yp.sku as sku
+      from ya_product yp, prod_region pr, ya_availability_override ao
+      where yp.sku = pr.prod_id 
+      and yp.is_prod_grp_parent = 'N'
+      and yp.account_id = ao.account_id
+      and pr.supplier_id = ao.supplier_id
+      and pr.is_enabled='Y'
+      and ao.start_date >= trunc(sysdate) - 1
+      and ao.start_date <= trunc(sysdate) + 1
+		union
+    select yp.sku as sku
+      from ya_product yp, prod_region pr, ya_availability_override ao
+      where yp.sku = pr.prod_id 
+      and yp.is_prod_grp_parent = 'N'
+      and yp.account_id = ao.account_id
+      and pr.supplier_id = ao.supplier_id
+      and pr.is_enabled='Y'
+      and ao.end_date >= trunc(sysdate) - 1
+      and ao.end_date <= trunc(sysdate) + 1
 	)
-	where sku not in (select sku from ya_se_reindex);
- 
+  where sku not in (select sku from ya_se_reindex);
+  
 	IF (SQLCODE = 0) THEN
 		COMMIT;
 	ELSE
