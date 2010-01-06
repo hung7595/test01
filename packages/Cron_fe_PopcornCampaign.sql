ee486eb1f31e030a73287e989628dd431481c4d8
michaelw <michaelw>
2010-01-06 03:06:11 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@1000 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- packages/Cron_fe_PopcornCampaign.sql
+++ packages/Cron_fe_PopcornCampaign.sql
@@ -27,7 +27,7 @@ CREATE OR REPLACE PACKAGE BODY "CRON_FE_POPCORNCAMPAIGN" AS
 			and region.is_enabled = 'Y'
 			and region.unit_cost / nvl(rate.exchange_rate, 1) < 6.2
 			and region.region_id = 1
-			and prod.account_id in (104,14,41,7,132,33,9,103,10,34,90,11,12,156,150,13,4,133,394,32,396,120,155,105,134,24,25,64,159,157,63,40,26)
+			and prod.account_id in (104,14,41,7,132,33,9,103,10,34,90,11,12,156,150,13,4,133,394,32,396,120,155,134,24,25,64,63,26)
 	) a,
 	(
 		select prod.sku as sku, case when sysdate between region.sale_price_start and region.sale_price_end and nvl(region.sale_price, 0) > 0 then region.sale_price else region.list_price end as sell_price 
@@ -43,7 +43,7 @@ CREATE OR REPLACE PACKAGE BODY "CRON_FE_POPCORNCAMPAIGN" AS
 			and region.is_enabled = 'Y'
 			and region.unit_cost / nvl(rate.exchange_rate, 1) < 6.2
 			and region.region_id = 7
-			and prod.account_id in (104,14,41,7,132,33,9,103,10,34,90,11,12,156,150,13,4,133,394,32,396,120,155,105,134,24,25,64,159,157,63,40,26)
+			and prod.account_id in (104,14,41,7,132,33,9,103,10,34,90,11,12,156,150,13,4,133,394,32,396,120,155,134,24,25,64,63,26)
 	) b
 	where a.sell_price > 9.99
 	and b.sell_price > 9.99
