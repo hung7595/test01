bc24784c4dfd9a1effdfa94f7a1954630bacd4a8
michaelw <michaelw>
2010-05-26 08:23:02 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@1158 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- materialized views/vw_size_info_sku.sql
+++ materialized views/vw_size_info_sku.sql
@@ -9,13 +9,11 @@ SELECT distinct pr.region_id, psi.lang_id, psi.size_info, psi.sku
   WHERE 1 = 1
   AND psi.sku = pa.prod_id
   AND pr.prod_id = pa.prod_id
-  AND pr.region_id in (10,11,13)
-  AND pa.region_id in (10,11,13)
+  AND pr.region_id in (10,11,13,14)
+  AND pa.region_id in (10,11,13,14)
   AND pa.avlb < 60
   AND pr.is_enabled = 'Y'
   AND pr.is_can_sell = 'Y'
   AND psi.lang_id in (1,2,5)
   AND pa.region_id = pr.region_id
 /
-
-grant select on VW_SIZE_INFO_SKU to v5_web_ysv2;
