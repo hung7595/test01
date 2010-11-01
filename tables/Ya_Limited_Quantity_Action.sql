6ce6d1d9794e3db483698f2e79eec3ca18e7e2de
jeffreyk <jeffreyk>
2010-11-01 03:09:08 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@1324 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- tables/Ya_Limited_Quantity_Action.sql
+++ tables/Ya_Limited_Quantity_Action.sql
@@ -17,3 +17,68 @@ USING INDEX TABLESPACE SSCommerceserver_Index
 
 CREATE PUBLIC SYNONYM Ya_Limited_Quantity_Action FOR Ya_Limited_Quantity_Action
 /
+
+
+CREATE OR REPLACE TRIGGER TrIn_limited_quantity_action
+  AFTER INSERT ON ya_limited_quantity_action
+  FOR EACH ROW
+DECLARE
+  iWrite                 NUMBER := 0;
+  isku                   NUMBER;
+  isite_id               NUMBER;
+  iaction_id             NUMBER;
+  isold_out_status       NUMBER;
+  vsuggestedSystem       varchar2(100) := 'FE_LimitedQuantity';
+  inormal_availability   NUMBER := 1;
+
+BEGIN
+
+  IF :new.action_id = 1 OR :new.action_id = 4 OR :new.action_id = 5 OR :new.action_id = 6/* mark soldout */ THEN
+	SELECT (CASE WHEN :new.action_id = 1 THEN 60
+	             WHEN :new.action_id = 4 THEN 70
+	             WHEN :new.action_id = 5 THEN 20
+	             WHEN :new.action_id = 6 THEN 30 END) INTO isold_out_status from dual;
+
+	IF :new.site_id = 99 THEN
+	  PKG_CATALOG_PROD_UPDT_QUEUE.sp_enqueue_prod_updt_queue(:new.sku,14,1,null,isold_out_status,vsuggestedSystem);
+	  PKG_CATALOG_PROD_UPDT_QUEUE.sp_enqueue_prod_updt_queue(:new.sku,14,4,null,isold_out_status,vsuggestedSystem);
+	  PKG_CATALOG_PROD_UPDT_QUEUE.sp_enqueue_prod_updt_queue(:new.sku,14,7,null,isold_out_status,vsuggestedSystem);
+	  PKG_CATALOG_PROD_UPDT_QUEUE.sp_enqueue_prod_updt_queue(:new.sku,14,8,null,isold_out_status,vsuggestedSystem);
+	ELSE
+	  PKG_CATALOG_PROD_UPDT_QUEUE.sp_enqueue_prod_updt_queue(:new.sku,14,:new.site_id,null,isold_out_status,vsuggestedSystem);
+	END IF;
+
+  ELSIF :new.action_id = 2 THEN
+	IF :new.site_id = 99 THEN
+	  PKG_CATALOG_PROD_UPDT_QUEUE.sp_enqueue_prod_updt_queue(:new.sku,2,1,null,null,vsuggestedSystem);
+	  PKG_CATALOG_PROD_UPDT_QUEUE.sp_enqueue_prod_updt_queue(:new.sku,3,1,null,null,vsuggestedSystem);
+	  PKG_CATALOG_PROD_UPDT_QUEUE.sp_enqueue_prod_updt_queue(:new.sku,4,1,null,null,vsuggestedSystem);
+	  PKG_CATALOG_PROD_UPDT_QUEUE.sp_enqueue_prod_updt_queue(:new.sku,2,4,null,null,vsuggestedSystem);
+	  PKG_CATALOG_PROD_UPDT_QUEUE.sp_enqueue_prod_updt_queue(:new.sku,3,4,null,null,vsuggestedSystem);
+	  PKG_CATALOG_PROD_UPDT_QUEUE.sp_enqueue_prod_updt_queue(:new.sku,4,4,null,null,vsuggestedSystem);
+	  PKG_CATALOG_PROD_UPDT_QUEUE.sp_enqueue_prod_updt_queue(:new.sku,2,7,null,null,vsuggestedSystem);
+	  PKG_CATALOG_PROD_UPDT_QUEUE.sp_enqueue_prod_updt_queue(:new.sku,3,7,null,null,vsuggestedSystem);
+	  PKG_CATALOG_PROD_UPDT_QUEUE.sp_enqueue_prod_updt_queue(:new.sku,4,7,null,null,vsuggestedSystem);
+	  PKG_CATALOG_PROD_UPDT_QUEUE.sp_enqueue_prod_updt_queue(:new.sku,2,8,null,null,vsuggestedSystem);
+	  PKG_CATALOG_PROD_UPDT_QUEUE.sp_enqueue_prod_updt_queue(:new.sku,3,8,null,null,vsuggestedSystem);
+	  PKG_CATALOG_PROD_UPDT_QUEUE.sp_enqueue_prod_updt_queue(:new.sku,4,8,null,null,vsuggestedSystem);
+	ELSE
+	  PKG_CATALOG_PROD_UPDT_QUEUE.sp_enqueue_prod_updt_queue(:new.sku,2,:new.site_id,null,null,vsuggestedSystem);
+	  PKG_CATALOG_PROD_UPDT_QUEUE.sp_enqueue_prod_updt_queue(:new.sku,3,:new.site_id,null,null,vsuggestedSystem);
+	  PKG_CATALOG_PROD_UPDT_QUEUE.sp_enqueue_prod_updt_queue(:new.sku,4,:new.site_id,null,null,vsuggestedSystem);
+	END IF;
+
+  ELSIF :new.action_id = 3 THEN
+	IF :new.site_id = 99 THEN
+	  PKG_CATALOG_PROD_UPDT_QUEUE.sp_enqueue_prod_updt_queue(:new.sku,11,1,null,'N',vsuggestedSystem);
+	  PKG_CATALOG_PROD_UPDT_QUEUE.sp_enqueue_prod_updt_queue(:new.sku,11,4,null,'N',vsuggestedSystem);
+	  PKG_CATALOG_PROD_UPDT_QUEUE.sp_enqueue_prod_updt_queue(:new.sku,11,7,null,'N',vsuggestedSystem);
+	  PKG_CATALOG_PROD_UPDT_QUEUE.sp_enqueue_prod_updt_queue(:new.sku,11,8,null,'N',vsuggestedSystem);
+	ELSE
+	  PKG_CATALOG_PROD_UPDT_QUEUE.sp_enqueue_prod_updt_queue(:new.sku,11,:new.site_id,null,'N',vsuggestedSystem);
+	END IF;
+
+  END IF;
+
+END;
+
