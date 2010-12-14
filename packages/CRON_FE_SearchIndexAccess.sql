dcc0d9036fc06310c021da66100cc666eef921c3
jeffreyk <jeffreyk>
2010-12-14 06:48:12 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@1362 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- packages/CRON_FE_SearchIndexAccess.sql
+++ packages/CRON_FE_SearchIndexAccess.sql
@@ -1,14 +1,15 @@
 CREATE OR REPLACE PACKAGE CRON_FE_SearchIndexAccess
 AS
   PROCEDURE CronJobUpdateSaleSku;
+  
+  PROCEDURE CronJobUpdateSaleSkuDuplicate;
+  
 END CRON_FE_SearchIndexAccess;
 /
 
 
-CREATE OR REPLACE PACKAGE BODY CRON_FE_SearchIndexAccess
-IS
+CREATE OR REPLACE PACKAGE BODY CRON_FE_SearchIndexAccess AS
  PROCEDURE CronJobUpdateSaleSku
-
  AS
  BEGIN
 	delete from ya_se_reindex where action = 1;
@@ -53,6 +54,53 @@ IS
 	END IF;
 
  END CronJobUpdateSaleSku;
+ 
+ PROCEDURE CronJobUpdateSaleSkuDuplicate
+ AS
+ BEGIN
+	delete from ya_se_reindex_duplicate where action = 1;
+	
+	insert into ya_se_reindex_duplicate (sku, action, mod_dt) 
+	select distinct sku, 0, sysdate from 
+	(
+		select prod_id as sku from prod_region where 
+		  sale_price_start >= trunc(sysdate) - 1
+		  and sale_price_start <= trunc(sysdate) + 1
+		union
+		select prod_id as sku  from prod_region where 
+		  sale_price_end >= trunc(sysdate) - 1
+		  and sale_price_end <= trunc(sysdate) + 1
+    union      
+    select yp.sku as sku
+      from ya_product yp, prod_region pr, ya_availability_override ao
+      where yp.sku = pr.prod_id 
+      and yp.is_prod_grp_parent = 'N'
+      and yp.account_id = ao.account_id
+      and pr.supplier_id = ao.supplier_id
+      and pr.is_enabled='Y'
+      and ao.start_date >= trunc(sysdate) - 1
+      and ao.start_date <= trunc(sysdate) + 1
+		union
+    select yp.sku as sku
+      from ya_product yp, prod_region pr, ya_availability_override ao
+      where yp.sku = pr.prod_id 
+      and yp.is_prod_grp_parent = 'N'
+      and yp.account_id = ao.account_id
+      and pr.supplier_id = ao.supplier_id
+      and pr.is_enabled='Y'
+      and ao.end_date >= trunc(sysdate) - 1
+      and ao.end_date <= trunc(sysdate) + 1
+	)
+  where sku not in (select sku from ya_se_reindex_duplicate);
+  
+	IF (SQLCODE = 0) THEN
+		COMMIT;
+	ELSE
+		ROLLBACK;
+	END IF;
+
+ END CronJobUpdateSaleSkuDuplicate;
+ 
 END CRON_FE_SearchIndexAccess;
 /
 
