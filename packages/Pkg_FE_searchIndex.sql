8a91dfc62b34129427695d912dc50b6a51c3c343
jeffreyk <jeffreyk>
2012-02-06 06:08:44 +0000

    no message
    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@1521 e0b5c6b3-c742-46a5-91d8-c7356876a33d

---
+++ packages/Pkg_FE_searchIndex.sql
@@ -0,0 +1,31 @@
+CREATE OR REPLACE PACKAGE Pkg_FE_searchIndex
+AS
+
+  PROCEDURE InsertSearchIndexProductId (
+  iPprodId IN INT
+  );
+
+  END Pkg_FE_searchIndex;
+/
+
+
+CREATE OR REPLACE PACKAGE BODY Pkg_FE_searchIndex
+IS
+
+PROCEDURE InsertSearchIndexProductId (
+  iPprodId IN INT
+  )
+  AS
+  BEGIN
+    delete from ya_se_reindex_duplicate where sku = iPprodId;
+	
+    insert into ya_se_reindex_duplicate (sku, action, mod_dt) 
+    select distinct sku, 0, sysdate from 
+    (
+      select sku from  ya_product where sku = iPprodId and is_prod_grp_parent = 'N'
+	);
+  
+  END InsertSearchIndexProductId;
+
+END Pkg_FE_searchIndex;
+/
