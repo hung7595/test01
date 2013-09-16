65a80a2fcef384dce5bbe74eb54a20f129a37b92
ch.ching <ch.ching>
2013-09-16 09:21:15 +0000

    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@1687 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- packages/PKG_FE_YwkBogoCouponCreation.sql
+++ packages/PKG_FE_YwkBogoCouponCreation.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE PACKAGE PKG_FE_YwkBogoCouponCreation
-AS  
+AS
   TYPE refCur IS REF CURSOR;
 
   PROCEDURE CreateBOGOCoupon (
@@ -30,9 +30,17 @@ AS
   AS
   BEGIN
 
-    INSERT INTO ya_coupon VALUES (iPcouponCode, iPcampaignName, iPcouponDescription, iPcouponAmount, null, 0, null, null, null, iPexpiredDate, iPshopperId, 'N', 'N', 1, 22, sys_guid(), 'Backend BOGO promotion coupon', sysdate, ipcouponcurrency, null, null);
+    INSERT INTO ya_coupon (COUPON_CODE,CAMPAIGN_NAME,COUPON_DESCRIPTION,DOLLAR_COUPON_VALUE,PERCENTAGE_COUPON_VALUE,
+                            ORDER_AMOUNT_TRIGGER,ORDER_QTY_TRIGGER,ITEM_SKU_TRIGGER,ITEM_QTY_TRIGGER,EXPIRATION_DATE,
+                            SHOPPER_ID,ALL_SHOPPERS,COUPON_USED,COUPON_TYPE_ID,SITE_ID,
+                            ROWGUID,CREATE_ID,CREATE_DATE,CURRENCY,UPDATE_ID,UPDATE_DATE)
+    VALUES (iPcouponCode, iPcampaignName, iPcouponDescription, iPcouponAmount, null,
+              0, null, null, null, iPexpiredDate,
+              iPshopperId, 'N', 'N', 1, 22,
+              sys_guid(), 'Backend BOGO promotion coupon', sysdate, ipcouponcurrency, null, null);
 
-    insert into ya_coupon_site values (iPcouponCode, 22);
+    INSERT INTO ya_coupon_site (coupon_code, site_id)
+    VALUES (iPcouponCode, 22);
 
     RETURN;
 
