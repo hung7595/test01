693a3ac30141c03ddf13c6c23a49a236b118049b
ch.ching <ch.ching>
2014-08-13 02:30:35 +0000

    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@1755 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- packages/CRON_FE_SearchIndexAccess.sql
+++ packages/CRON_FE_SearchIndexAccess.sql
@@ -1,9 +1,9 @@
 CREATE OR REPLACE PACKAGE CRON_FE_SearchIndexAccess
 AS
   PROCEDURE CronJobUpdateSaleSku;
-  
+
   PROCEDURE CronJobUpdateSaleSkuDuplicate;
-  
+
 END CRON_FE_SearchIndexAccess;
 /
 
@@ -14,20 +14,20 @@ CREATE OR REPLACE PACKAGE BODY CRON_FE_SearchIndexAccess AS
  BEGIN
 	delete from ya_se_reindex where action = 1;
 	
-	insert into ya_se_reindex (sku, action, mod_dt) 
-	select distinct sku, 0, sysdate from 
+	insert into ya_se_reindex (sku, action, mod_dt)
+	select distinct sku, 0, sysdate from
 	(
-		select prod_id as sku from prod_region where 
+		select prod_id as sku from prod_region where
 		  sale_price_start >= trunc(sysdate) - 1
 		  and sale_price_start <= trunc(sysdate) + 1
 		union
-		select prod_id as sku  from prod_region where 
+		select prod_id as sku  from prod_region where
 		  sale_price_end >= trunc(sysdate) - 1
 		  and sale_price_end <= trunc(sysdate) + 1
-    union      
+    union
     select yp.sku as sku
       from ya_product yp, prod_region pr, ya_availability_override ao
-      where yp.sku = pr.prod_id 
+      where yp.sku = pr.prod_id
       and yp.is_prod_grp_parent = 'N'
       and yp.account_id = ao.account_id
       and pr.supplier_id = ao.supplier_id
@@ -37,7 +37,7 @@ CREATE OR REPLACE PACKAGE BODY CRON_FE_SearchIndexAccess AS
 		union
     select yp.sku as sku
       from ya_product yp, prod_region pr, ya_availability_override ao
-      where yp.sku = pr.prod_id 
+      where yp.sku = pr.prod_id
       and yp.is_prod_grp_parent = 'N'
       and yp.account_id = ao.account_id
       and pr.supplier_id = ao.supplier_id
@@ -46,7 +46,7 @@ CREATE OR REPLACE PACKAGE BODY CRON_FE_SearchIndexAccess AS
       and ao.end_date <= trunc(sysdate) + 1
 	)
   where sku not in (select sku from ya_se_reindex);
-  
+
 	IF (SQLCODE = 0) THEN
 		COMMIT;
 	ELSE
@@ -54,45 +54,63 @@ CREATE OR REPLACE PACKAGE BODY CRON_FE_SearchIndexAccess AS
 	END IF;
 
  END CronJobUpdateSaleSku;
- 
+
  PROCEDURE CronJobUpdateSaleSkuDuplicate
  AS
  BEGIN
 	delete from ya_se_reindex_duplicate where action = 1;
 	
-	insert into ya_se_reindex_duplicate (sku, action, mod_dt) 
-	select distinct sku, 0, sysdate from 
+	insert into ya_se_reindex_duplicate (sku, action, mod_dt)
+	select distinct sku, 0, sysdate from
 	(
-		select prod_id as sku from prod_region where 
+		select prod_id as sku from prod_region where
 		  sale_price_start >= trunc(sysdate) - 1
 		  and sale_price_start <= trunc(sysdate) + 1
 		union
-		select prod_id as sku  from prod_region where 
+		select prod_id as sku  from prod_region where
 		  sale_price_end >= trunc(sysdate) - 1
 		  and sale_price_end <= trunc(sysdate) + 1
-    union      
+    union
     select yp.sku as sku
-      from ya_product yp, prod_region pr, ya_availability_override ao
-      where yp.sku = pr.prod_id 
-      and yp.is_prod_grp_parent = 'N'
-      and yp.account_id = ao.account_id
-      and pr.supplier_id = ao.supplier_id
-      and pr.is_enabled='Y'
-      and ao.start_date >= trunc(sysdate) - 1
-      and ao.start_date <= trunc(sysdate) + 1
+      from ya_product yp
+        inner join prod_region pr on yp.sku = pr.prod_id and pr.region_id in (1,7,12)
+        inner join ya_availability_override ao on yp.account_id = ao.account_id and pr.supplier_id = ao.supplier_id
+      where yp.is_prod_grp_parent = 'N'
+        and pr.is_enabled='Y'
+        and ao.start_date >= trunc(sysdate) - 1
+        and ao.start_date <= trunc(sysdate) + 1
 		union
     select yp.sku as sku
-      from ya_product yp, prod_region pr, ya_availability_override ao
-      where yp.sku = pr.prod_id 
-      and yp.is_prod_grp_parent = 'N'
-      and yp.account_id = ao.account_id
-      and pr.supplier_id = ao.supplier_id
-      and pr.is_enabled='Y'
-      and ao.end_date >= trunc(sysdate) - 1
-      and ao.end_date <= trunc(sysdate) + 1
+      from ya_product yp
+        inner join prod_region pr on yp.sku = pr.prod_id and pr.region_id in (1,7,12)
+        inner join ya_availability_override ao on yp.account_id = ao.account_id and pr.supplier_id = ao.supplier_id
+      where yp.is_prod_grp_parent = 'N'
+        and pr.is_enabled='Y'
+        and ao.end_date >= trunc(sysdate) - 1
+        and ao.end_date <= trunc(sysdate) + 1
+    union
+    select yp.sku as sku
+      from ya_product yp
+        inner join prod_region pr on yp.sku = pr.prod_id and pr.region_id in (10,13,14,15,18)
+        inner join ya_availability_override ao on yp.account_id = ao.account_id and pr.supplier_id = ao.supplier_id
+      where not exists (select 1 from ya_product_title_rel ptr where ptr.product_title_child_sku = yp.sku)
+        and yp.is_prod_grp_parent = 'N'
+        and pr.is_enabled='Y'
+        and ao.start_date >= trunc(sysdate) - 1
+        and ao.start_date <= trunc(sysdate) + 1
+		union
+    select yp.sku as sku
+      from ya_product yp
+        inner join prod_region pr on yp.sku = pr.prod_id and pr.region_id in (10,13,14,15,18)
+        inner join ya_availability_override ao on yp.account_id = ao.account_id and pr.supplier_id = ao.supplier_id
+      where not exists (select 1 from ya_product_title_rel ptr where ptr.product_title_child_sku = yp.sku)
+        and yp.is_prod_grp_parent = 'N'
+        and pr.is_enabled='Y'
+        and ao.end_date >= trunc(sysdate) - 1
+        and ao.end_date <= trunc(sysdate) + 1
 	)
   where sku not in (select sku from ya_se_reindex_duplicate);
-  
+
 	IF (SQLCODE = 0) THEN
 		COMMIT;
 	ELSE
@@ -100,7 +118,7 @@ CREATE OR REPLACE PACKAGE BODY CRON_FE_SearchIndexAccess AS
 	END IF;
 
  END CronJobUpdateSaleSkuDuplicate;
- 
+
 END CRON_FE_SearchIndexAccess;
 /
 
