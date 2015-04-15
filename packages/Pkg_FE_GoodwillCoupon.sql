23873a934041c24957b35d872c24bfb9ed781b78
sykeung <sykeung>
2015-04-15 05:58:53 +0000

    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@1795 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- packages/Pkg_FE_GoodwillCoupon.sql
+++ packages/Pkg_FE_GoodwillCoupon.sql
@@ -12,6 +12,13 @@ AS
       cLcurrency IN VARCHAR2,
       cPcouponCode OUT VARCHAR2
   );
+
+  PROCEDURE CreateGoodwillCouponByOrderNum (
+      iPorderNum IN INT,
+      nPcouponAmt IN NUMBER,
+      cPcurrency IN VARCHAR2,
+      cPcouponCode OUT VARCHAR2
+  );
   
 END PKG_FE_GOODWILLCOUPON;
 /
@@ -155,6 +162,72 @@ IS
                             VALUES (cPcouponCode, iLsiteId);
         COMMIT;
   END CreateGoodwillCouponCurrency;
+
+  PROCEDURE CreateGoodwillCouponByOrderNum (
+      iPorderNum IN INT,
+      nPcouponAmt IN NUMBER,
+      cPcurrency VARCHAR2,
+      cPcouponCode OUT VARCHAR2
+  )
+  AS
+    iLsiteId INT;
+    cLshopperId CHAR(32);
+    cLcampaignName VARCHAR2(50);
+    cLdescription VARCHAR2(255);
+    nLtrigger INT;
+    dtLexpire DATE;
+    cLallShoppers CHAR(1);
+    cLused CHAR(1);
+    iLtype INT;
+    cLcreateUser VARCHAR2(50);
+    dtLcreateDate DATE;
+    iLaccountType INT;
+    iLcountCount INT;
+    cLcodePrefix VARCHAR2(15);
+  BEGIN
+    /*
+      preload coupon setting from order
+    */
+    SELECT origin_id, cust_id INTO iLsiteId, cLshopperId
+    FROM order_info oi
+      INNER JOIN billing_info bi ON oi.id = bi.order_info_id
+    WHERE oi.origin_order_id = CAST(iPorderNum as VARCHAR2(10))
+      AND ROWNUM = 1;
+
+    /*
+      coupon setting
+    */
+    cLcreateUser := 'Backend Goodwill';
+    dtLcreateDate := SYSDATE;
+    cLcampaignName := 'HKCS Goodwill';
+    cLdescription := 'Goodwill coupon';
+    nLtrigger := 0;
+    dtLexpire := add_months(dtLcreateDate, 2);
+    cLallShoppers := 'N';
+    cLused := 'N';
+    iLtype := 1;
+    iLaccountType := 0;
+    
+    /*
+      generate coupon code
+    */
+    cLcodePrefix := 'GW' || iPorderNum;
+    SELECT COUNT(1) INTO iLcountCount FROM ya_coupon WHERE coupon_code like (cLcodePrefix || '%');
+    SELECT cLcodePrefix || '_' || CAST(iLcountCount + 1 as VARCHAR2(2)) INTO cPcouponCode FROM dual;
+
+    /*
+      create coupon
+    */
+    INSERT INTO ya_coupon   (coupon_code, campaign_name, coupon_description, dollar_coupon_value, order_amount_trigger
+                          , expiration_date, shopper_id, all_shoppers, coupon_used, coupon_type_id
+                          , site_id, create_id, create_date, currency, account_type_id)
+                    VALUES (cPcouponCode, cLcampaignName, cLdescription, nPcouponAmt, nLtrigger
+                          , dtLexpire, cLshopperId, cLallShoppers, cLused, iLtype
+                          , iLsiteId, cLcreateUser, dtLcreateDate, cPcurrency, iLaccountType);  
+
+    INSERT INTO ya_coupon_site (coupon_code, site_id)
+                        VALUES (cPcouponCode, iLsiteId);
+  END CreateGoodwillCouponByOrderNum;
   
 END Pkg_FE_GoodwillCoupon;
 /
