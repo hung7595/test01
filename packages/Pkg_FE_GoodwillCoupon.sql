a5d3aa22b4c1ea35dbc8b0ec49cafb3a361011f3
ch.ching <ch.ching>
2015-11-23 08:53:56 +0000

    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@1813 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- packages/Pkg_FE_GoodwillCoupon.sql
+++ packages/Pkg_FE_GoodwillCoupon.sql
@@ -20,6 +20,14 @@ AS
       cPcouponCode OUT VARCHAR2
   );
   
+  PROCEDURE CreateGWCouponByOrderNumNExpDt (
+      iPorderNum IN INT,
+      nPcouponAmt IN NUMBER,
+      cPcurrency IN VARCHAR2,
+      dtPexpireDate IN Date,
+      cPcouponCode OUT VARCHAR2
+  );
+  
   PROCEDURE CreateInferiorGoodwillCoupon (
       iPorderNum IN INT,
       nPcouponAmt IN NUMBER,
@@ -237,6 +245,72 @@ IS
     INSERT INTO ya_coupon_site (coupon_code, site_id)
                         VALUES (cPcouponCode, iLsiteId);
   END CreateGoodwillCouponByOrderNum;
+  
+  PROCEDURE CreateGWCouponByOrderNumNExpDt (
+      iPorderNum IN INT,
+      nPcouponAmt IN NUMBER,
+      cPcurrency IN VARCHAR2,
+      dtPexpireDate IN Date,
+      cPcouponCode OUT VARCHAR2
+  )
+  AS
+    iLsiteId INT;
+    cLshopperId CHAR(32);
+    cLcampaignName VARCHAR2(50);
+    cLdescription VARCHAR2(255);
+    nLtrigger INT;
+    dtLexpire DATE;
+    cLallShoppers CHAR(1);
+    cLused CHAR(1);
+    iLtype INT;
+    cLcreateUser VARCHAR2(50);
+    dtLcreateDate DATE;
+    iLaccountType INT;
+    iLcountCount INT;
+    cLcodePrefix VARCHAR2(15);
+  BEGIN
+    /*
+      preload coupon setting from order
+    */
+    SELECT origin_id, cust_id INTO iLsiteId, cLshopperId
+    FROM order_info oi
+      INNER JOIN billing_info bi ON oi.id = bi.order_info_id
+    WHERE oi.origin_order_id = CAST(iPorderNum as VARCHAR2(10))
+      AND ROWNUM = 1;
+
+    /*
+      coupon setting
+    */
+    cLcreateUser := 'Backend Goodwill';
+    dtLcreateDate := SYSDATE;
+    cLcampaignName := 'HKCS Goodwill';
+    cLdescription := 'Goodwill coupon';
+    nLtrigger := 0;
+    cLallShoppers := 'N';
+    cLused := 'N';
+    iLtype := 1;
+    iLaccountType := 0;
+    
+    /*
+      generate coupon code
+    */
+    cLcodePrefix := 'GW' || iPorderNum;
+    SELECT COUNT(1) INTO iLcountCount FROM ya_coupon WHERE coupon_code like (cLcodePrefix || '%');
+    SELECT cLcodePrefix || '_' || CAST(iLcountCount + 1 as VARCHAR2(2)) INTO cPcouponCode FROM dual;
+
+    /*
+      create coupon
+    */
+    INSERT INTO ya_coupon   (coupon_code, campaign_name, coupon_description, dollar_coupon_value, order_amount_trigger
+                          , expiration_date, shopper_id, all_shoppers, coupon_used, coupon_type_id
+                          , site_id, create_id, create_date, currency, account_type_id)
+                    VALUES (cPcouponCode, cLcampaignName, cLdescription, nPcouponAmt, nLtrigger
+                          , dtPexpireDate, cLshopperId, cLallShoppers, cLused, iLtype
+                          , iLsiteId, cLcreateUser, dtLcreateDate, cPcurrency, iLaccountType);  
+
+    INSERT INTO ya_coupon_site (coupon_code, site_id)
+                        VALUES (cPcouponCode, iLsiteId);
+  END CreateGWCouponByOrderNumNExpDt;
   
   /*
     generate inferior goodwill coupon
