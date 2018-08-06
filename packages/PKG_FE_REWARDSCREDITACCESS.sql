e800f936d8ecff82fda241e8fd0a6f144e9a3151
ceci.cheung <ceci.cheung>
2018-08-06 02:27:28 +0000

    git-svn-id: http://svn.yawin.yesasia.com:1888/svn/database/oracle/trunk/yarac/ss_adm@1918 e0b5c6b3-c742-46a5-91d8-c7356876a33d

--- packages/PKG_FE_REWARDSCREDITACCESS.sql
+++ packages/PKG_FE_REWARDSCREDITACCESS.sql
@@ -1,6 +1,7 @@
 CREATE OR REPLACE package Pkg_fe_RewardsCreditAccess
 AS
     TYPE refCur IS REF CURSOR;
+    TYPE charInRewardsCode IS VARRAY(32) OF VARCHAR(1);
 
     PROCEDURE CreateRewardsCode (
         cPshopper_id IN CHAR,
@@ -46,7 +47,10 @@ IS
     AS
         iLrecord_found INT;
         iLrewards_code_exist INT;
+        iArray charInRewardsCode;
     BEGIN
+        iArray := charInRewardsCode('2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
+
         SELECT COUNT(1) INTO iLrecord_found FROM YA_SHOPPER WHERE shopper_id = cPshopper_id;
 
         IF iLrecord_found = 0 THEN
@@ -61,16 +65,21 @@ IS
             RETURN;
         END IF;
 
-        iLrecord_found := 0;
+        iLrecord_found := 1;
 
-        LOOP
-            cPrewardsCode := dbms_random.string('X', 6);
+        WHILE iLrecord_found = 1 LOOP
+            cPrewardsCode := iArray(dbms_random.value(0,32)) ||
+                             iArray(dbms_random.value(0,32)) ||
+                             iArray(dbms_random.value(0,32)) ||
+                             iArray(dbms_random.value(0,32)) ||
+                             iArray(dbms_random.value(0,32)) ||
+                             iArray(dbms_random.value(0,32));
+
             SELECT COUNT(*) INTO iLrecord_found FROM YA_SHOPPER WHERE rewards_code = cPrewardsCode;
-        EXIT WHEN (iLrecord_found = 0 AND NOT (InStr(cPrewardsCode, '0') > 0 OR InStr(cPrewardsCode, '1') > 0 OR InStr(cPrewardsCode, 'I') > 0 OR InStr(cPrewardsCode, 'O') > 0));
         END LOOP;
 
         UPDATE YA_SHOPPER
-        SET rewards_code = cPrewardsCode
+        SET rewards_code = cPrewardsCode, updated_date = SYSDATE
         WHERE shopper_id = cPshopper_id;
 
         iPerror_code := 0;
@@ -91,6 +100,7 @@ IS
         iPerror_code OUT INT
     )
     AS
+        iLaction_id INT;
         iLrecord_found INT;
         iLcredit_id INT;
     BEGIN
@@ -106,13 +116,20 @@ IS
             RETURN;
         END IF;
 
+        --
+        CASE iPcredit_type_id
+            WHEN 1 THEN iLaction_id := 1;
+            WHEN 2 THEN iLaction_id := 3;
+            WHEN 3 THEN iLaction_id := 5;
+        END CASE;
+
         SELECT seq_rewards_credit.NEXTVAL INTO iLcredit_id FROM DUAL;
 
         INSERT INTO ya_rewards_credit(id, site_id, shopper_id, credit_type_id, credit_discount, initial_balance, current_balance, transaction_datetime, bogus, remark, currency, expiry_date, create_user, create_dt, mod_user, mod_dt)
         VALUES (iLcredit_id, iPsite_id, sPshopper_id, iPcredit_type_id, deciPdiscount, nPamount, nPamount, SYSDATE, 'N', cPremark, cPcurrency, iPexpiryDate, cPupdatedUser, SYSDATE, null, null);
 
         INSERT INTO ya_rewards_credit_txn(transaction_id, credit_id, credit_amount, credit_ordernum, debit_amount, debit_ordernum, snapshot_balance, transaction_datetime, transaction_remark, action_id, create_user, create_dt, mod_user, mod_dt)
-        VALUES (SYS_GUID(), iLcredit_id, nPamount, iPorder_num, null, null, nPamount, SYSDATE, cPremark, 1, cPupdatedUser, SYSDATE, null, null);
+        VALUES (SYS_GUID(), iLcredit_id, nPamount, iPorder_num, null, null, nPamount, SYSDATE, cPremark, iLaction_id, cPupdatedUser, SYSDATE, null, null);
 
         iPerror_code := 0;
         COMMIT;
@@ -154,7 +171,10 @@ IS
                 OPEN curLdeduct_cursor FOR
                 SELECT id, current_balance
                 FROM ya_rewards_credit
-                WHERE shopper_id = cPshopper_id AND currency = cPcurrency AND site_id = iPsite_id
+                WHERE shopper_id = cPshopper_id
+                    AND currency = cPcurrency
+                    AND site_id = iPsite_id
+                    AND current_balance > 0
                 ORDER BY expiry_date ASC;
 
 
@@ -196,20 +216,22 @@ IS
                         iLcredit_id,
                         null,
                         null,
-                        deciPdebit_amount,
+                        deciLdeduct_amount,
                         iPorder_num,
                         deciLcurrent_balance - deciLdeduct_amount,
                         SYSDATE,
                         null,
-                        2,
+                        2, -- REDEEM
                         cPupdatedUser,
                         SYSDATE,
-                        null,
-                        null
+                        null, -- modCreate
+                        null -- modDate
                     );
 
                     UPDATE YA_REWARDS_CREDIT
-                    SET current_balance = deciLcurrent_balance - deciLdeduct_amount
+                    SET current_balance = deciLcurrent_balance - deciLdeduct_amount,
+                        mod_user = cPupdatedUser,
+                        mod_dt = SYSDATE
                     WHERE id = iLcredit_id;
 
                     FETCH curLdeduct_cursor INTO iLcredit_id, deciLcurrent_balance;
